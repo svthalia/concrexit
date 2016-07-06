ce9b4be4ef75fc0c193de73ce5a283377efef389
Thom Wiggers <thom@thomwiggers.nl>
2016-07-06 15:48:52 +0200

    Include settings for admin site

---
+++ website/thaliawebsite/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+
+admin.site.site_header = _('Thalia administratie')
+admin.site.site_title = _('Thalia')
--- website/thaliawebsite/settings.py
+++ website/thaliawebsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Our apps
+    'thaliawebsite',  # include for admin settings
     'members',
 ]
 
