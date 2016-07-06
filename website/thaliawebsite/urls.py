3fec655e113e57dba096e204f83d22c737eb1ead
Luuk Scholten <info@luukscholten.com>
2016-07-06 23:04:27 +0200

    Add rough version of template

--- website/thaliawebsite/urls.py
+++ website/thaliawebsite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^members/', include('members.urls')),
     # Default login helpers
