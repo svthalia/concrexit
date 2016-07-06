3e03b1f1220f6a7c621834056550a1b49850550e
Thom Wiggers <thom@thomwiggers.nl>
2016-07-06 15:49:39 +0200

    Use English translation

--- website/thaliawebsite/admin.py
+++ website/thaliawebsite/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 
-admin.site.site_header = _('Thalia administratie')
+admin.site.site_header = _('Thalia administration')
 admin.site.site_title = _('Thalia')
