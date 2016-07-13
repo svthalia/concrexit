8a7c7c4302da820535b35a10496ec0c4668979b8
Luuk Scholten <info@luukscholten.com>
2016-07-13 19:58:58 +0200

    Add menu outline with not yet implemented pages

---
+++ website/thaliawebsite/menus.py
@@ -0,0 +1,33 @@
+from django.utils.translation import ugettext as _
+
+main = [
+    {'title': _('Home'), 'name': 'index'},
+    {'title': _('Association'), 'name': '#', 'submenu': [
+        {'title': _('Board'), 'name': '#'},
+        {'title': _('Committees'), 'name': '#'},
+        {'title': _('Members'), 'name': '#'},
+        {'title': _('Documents'), 'name': '#'},
+        {'title': _('Members'), 'name': '#'},
+        {'title': _('Sister Associations'), 'name': '#'},
+        {'title': _('Become Member'), 'name': '#'},
+        {'title': _('Thabloid'), 'name': '#'},
+    ]},
+    {'title': _('For Members'), 'name': '#', 'submenu': [
+        {'title': _('Photos'), 'name': '#'},
+        {'title': _('Statistics'), 'name': '#'},
+        {'title': _('Become Active'), 'name': '#'},
+        {'title': _('Wiki'), 'name': '#'},
+    ]},
+    {'title': _('Calendar'), 'name': '#'},
+    {'title': _('Career'), 'name': '#', 'submenu': [
+        {'title': _('Sponsor'), 'name': '#'},
+        {'title': _('Vacancies'), 'name': '#'},
+    ]},
+    {'title': _('Education'), 'name': '#', 'submenu': [
+        {'title': _('Book Sale'), 'name': '#'},
+        {'title': _('Course Overview'), 'name': '#'},
+        {'title': _('Add Exam'), 'name': '#'},
+        {'title': _('Add Summary'), 'name': '#'},
+    ]},
+    {'title': _('Contact'), 'name': '#'},
+]
--- website/thaliawebsite/urls.py
+++ website/thaliawebsite/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^members/', include('members.urls')),
+    url(r'^nyi$', TemplateView.as_view(template_name='status/nyi.html'), name='#'),
     # Default login helpers
     url(r'^', include('django.contrib.auth.urls')),
 ]
