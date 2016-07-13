74f24ff57fa98d525a262c40152dfbca59e967f5
Thom Wiggers <thom@thomwiggers.nl>
2016-07-13 20:39:15 +0200

    taal is moeilijk
    [ci skip]

--- source_me.sh
+++ source_me.sh
@@ -2,8 +2,7 @@
 pathname=$_
 
 if [[ "$pathname" != "$0" ]]; then
-    echo "Rember: you need to run me as 'source ./source_me.sh', execute it!"
-    echo "So if shit doesn't work, try that (you may have alrd"
+    echo "Remember: you need to run me as 'source ./source_me.sh', execute it!"
     return
 fi
 
