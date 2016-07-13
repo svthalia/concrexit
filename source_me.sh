10a4ce8b0f9419dce53dedca29a000feea6b8ebf
Thom Wiggers <thom@thomwiggers.nl>
2016-07-13 20:06:53 +0200

    only complain if not sourced

--- source_me.sh
+++ source_me.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+pathname=$_
 
-echo "Rember: you need to run me as 'source ./source_me.sh', not as './source_me.sh'!"
-echo "So if shit doesn't work, try that (you may have alrd"
+if [[ "$pathname" != "$0" ]]; then
+    echo "Rember: you need to run me as 'source ./source_me.sh', execute it!"
+    echo "So if shit doesn't work, try that (you may have alrd"
+    return
+fi
 
 if [ -d venv ]; then
     source venv/bin/activate
