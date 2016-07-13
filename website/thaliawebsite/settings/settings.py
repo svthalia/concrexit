e90029b61c12e20cf919dd6a0269e3e1eefcc4f2
Thom Wiggers <thom@thomwiggers.nl>
2016-07-13 21:07:35 +0200

    fix BASE_PATH

--- website/thaliawebsite/settings/settings.py
+++ website/thaliawebsite/settings/settings.py
@@ -14,8 +14,9 @@ import os
 from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    '..', '..'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
