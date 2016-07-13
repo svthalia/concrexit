e2df81b08336988da2822afb1d937f8072ebf9f1
Thom Wiggers <thom@thomwiggers.nl>
2016-07-13 22:36:22 +0200

    Fix source-me

--- source_me.sh
+++ source_me.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pathname=$_
 
-if [[ "$pathname" != "$0" ]]; then
-    echo "Remember: you need to run me as 'source ./source_me.sh', execute it!"
+if [[ "$pathname" = "$0" ]]; then
+    echo "Remember: you need to run me as 'source ./source_me.sh', not execute it!"
     return
 fi
 
