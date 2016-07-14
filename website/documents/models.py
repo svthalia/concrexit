20669eea53c6478da5499c044a16a2219ee33747
Joost Rijneveld <joost@joostrijneveld.nl>
2016-07-14 22:47:44 +0200

    Greatly simplify file storage
    There is no need to make it complex:
    - Human-readable is unnecessary and difficult
    - Django takes care of uniqueness just fine
    - The filesystem does not mind lots of files in 1 dir

--- website/documents/models.py
+++ website/documents/models.py
@@ -1,37 +1,19 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-import os
-
 from utils.validators import validate_file_extension
 
 
-def associationdocs_path(instance, filename):
-    filename = instance.filetype + instance.year
-    return 'documents/associationdocs/{0}'.format(filename)
-
-
-def minutes_path(instance, filename):
-    _, ext = os.path.splitext(filename)
-    return 'documents/{}/minutes{}'.format(instance.datetime.date(), ext)
-
-
-def general_meetingdocs_path(instance, filename):
-    _, ext = os.path.splitext(filename)
-    return 'documents/{}/files/'.format(instance.datetime.date(), filename)
-
-
 class AssociationDocument(models.Model):
-    name = models.CharField(max_length=200)
     year = models.IntegerField()
     file = models.FileField(
-        upload_to=associationdocs_path,
+        upload_to='documents/association/',
         validators=[validate_file_extension],
     )
     FILETYPES = (
-        ('policy_document', _("Policy document")),
-        ('annual_report', _("Annual report")),
-        ('financial_report', _("Financial report")),
+        ('policy-document', _("Policy document")),
+        ('annual-report', _("Annual report")),
+        ('financial-report', _("Financial report")),
     )
     filetype = models.CharField(
         max_length=16,
@@ -42,7 +24,7 @@ class AssociationDocument(models.Model):
 class GenericDocument(models.Model):
     name = models.CharField(max_length=200)
     file = models.FileField(
-        upload_to='documents/general/',
+        upload_to='documents/generic/',
         validators=[validate_file_extension],
     )
     members_only = models.BooleanField(default=False)
@@ -53,7 +35,7 @@ class GenericDocument(models.Model):
 
 class GeneralMeeting(models.Model):
     minutes = models.FileField(
-        upload_to=minutes_path,
+        upload_to='documents/meetings/minutes/',
         validators=[validate_file_extension],
     )
     datetime = models.DateTimeField()
@@ -63,7 +45,7 @@ class GeneralMeeting(models.Model):
 class GeneralMeetingDocument(models.Model):
     meeting = models.ForeignKey(GeneralMeeting, on_delete=models.CASCADE)
     file = models.FileField(
-        upload_to=general_meetingdocs_path,
+        upload_to='documents/meetings/files/',
         validators=[validate_file_extension],
     )
 
