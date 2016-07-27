7fcd16e3fb24fa99dd564d0a2465823a47e99e37
Joost Rijneveld <joost@joostrijneveld.nl>
2016-07-27 22:21:29 +0200

    Translate menu (and misc)

--- website/thaliawebsite/menus.py
+++ website/thaliawebsite/menus.py
@@ -9,7 +9,7 @@ main = [
         {'title': _('Documents'), 'name': 'documents:index'},
         {'title': _('Members'), 'name': '#'},
         {'title': _('Sister Associations'), 'name': 'sister-associations'},
-        {'title': _('Become Member'), 'name': '#'},
+        {'title': _('Become a Member'), 'name': '#'},
         {'title': _('Thabloid'), 'name': '#'},
     ]},
     {'title': _('For Members'), 'name': '#', 'submenu': [
@@ -20,14 +20,14 @@ main = [
     ]},
     {'title': _('Calendar'), 'name': '#'},
     {'title': _('Career'), 'name': '#', 'submenu': [
-        {'title': _('Sponsor'), 'name': '#'},
+        {'title': _('Sponsors'), 'name': '#'},
         {'title': _('Vacancies'), 'name': '#'},
     ]},
     {'title': _('Education'), 'name': '#', 'submenu': [
         {'title': _('Book Sale'), 'name': '#'},
         {'title': _('Course Overview'), 'name': '#'},
-        {'title': _('Add Exam'), 'name': '#'},
-        {'title': _('Add Summary'), 'name': '#'},
+        {'title': _('Submit Exam'), 'name': '#'},
+        {'title': _('Submit Summary'), 'name': '#'},
     ]},
     {'title': _('Contact'), 'name': 'contact'},
 ]
