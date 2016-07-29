01967c44991434dc98bb9582b3016fa60a929876
Joost Rijneveld <joost@joostrijneveld.nl>
2016-07-29 21:34:50 +0200

    Refactor Model to abstract from file system ops

---
+++ website/photos/models.py
@@ -0,0 +1,46 @@
+from django.utils.text import slugify
+from django.conf import settings
+from django.utils.functional import cached_property
+
+import os
+import re
+from datetime import datetime
+import random
+
+COVER_FILENAME = 'cover.jpg'
+
+
+class Album(object):
+
+    photosdir = 'photos'
+    photospath = os.path.join(settings.MEDIA_ROOT, photosdir)
+
+    def __init__(self, dirname):
+        date, self.title = dirname.split('_')
+        # This supports both 1990-11-07_albumname as well as 19901107_albumname
+        self.date = datetime.strptime(re.sub(r"\D", "", date), '%Y%m%d')
+        self.dirname = dirname
+        self.path = os.path.join(Album.photospath, self.dirname)
+
+    @cached_property
+    def slug(self):
+        return slugify('{}-{}'.format(self.date.strftime('%Y-%m-%d'),
+                                      self.title))
+
+    @cached_property
+    def cover(self):
+        if os.path.isfile(os.path.join(self.path, COVER_FILENAME)):
+            cover = COVER_FILENAME
+        else:
+            random.seed(self.dirname)
+            cover = random.choice(os.listdir(self.path))
+        return os.path.join(Album.photosdir, self.dirname, cover)
+
+    @cached_property
+    def photos(self):
+        return [os.path.join(Album.photosdir, self.dirname, photo)
+                for photo in os.listdir(self.path) if photo != COVER_FILENAME]
+
+    @classmethod
+    def all(cls):
+        return [cls(dirname) for dirname in os.listdir(Album.photospath)]
--- website/photos/views.py
+++ website/photos/views.py
@@ -1,61 +1,26 @@
 from django.shortcuts import render
-from django.conf import settings
-from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.utils.translation import ugettext_lazy as _
 
-import os
-import re
-import random
-from datetime import datetime
+from .models import Album
 
 COVER_FILENAME = 'cover.jpg'
 
 
-# TODO consider refactoring this to models.py instead, for separation.
-def _album_dict(photodir):
-    """Creates a dict with album information based on a parent directory."""
-    albums = [(name, name.split('_')) for name in os.listdir(photodir)]
-    albums = [{'date': datetime.strptime(re.sub(r"\D", "", date), '%Y%m%d'),
-               'title': title,
-               'name': name,
-               } for name, (date, title) in albums]
-    for album in albums:
-        album['slug'] = slugify('{}-{}'
-                                .format(album['date'].strftime('%Y-%m-%d'),
-                                        album['title']))
-    return albums
-
-
 @login_required
 def index(request):
-    photodir = os.path.join(settings.MEDIA_ROOT, 'photos')
-    albums = _album_dict(photodir)
-
-    for album in albums:
-        albumdir = os.path.join(photodir, album['name'])
-        if os.path.isfile(os.path.join(albumdir, COVER_FILENAME)):
-            album['cover'] = COVER_FILENAME
-        else:
-            random.seed(album['name'])
-            album['cover'] = random.choice(os.listdir(albumdir))
-        album['cover'] = os.path.join('photos',
-                                      album['name'],
-                                      album['cover'])
-
-    return render(request, 'photos/index.html', {'albums': albums})
+    return render(request, 'photos/index.html', {'albums': Album.all()})
 
 
 @login_required
 def album(request, slug):
-    photodir = os.path.join(settings.MEDIA_ROOT, 'photos')
-    for album in _album_dict(photodir):
-        if album['slug'] == slug:
+    for album in Album.all():
+        if album.slug == slug:
             break
-    albumdir = os.path.join(photodir, album['name'])
-    photos = [os.path.join('photos', album['name'], photo)
-              for photo in os.listdir(albumdir) if photo != COVER_FILENAME]
-
-    return render(request, 'photos/album.html', {'photos': photos})
+    else:
+        raise Http404(_("Album not found."))
+    return render(request, 'photos/album.html', {'album': album})
 
 
 @login_required
