d5194137445370a4416fdd874ad6d17b0866eb0e
Jan <jan-martens@hotmail.com>
2016-08-05 16:37:21 +0200

    util function datetime_to_lectureyear works on both datetime and datetime.date objects

--- website/utils/snippets.py
+++ website/utils/snippets.py
@@ -6,7 +6,9 @@ import os
 
 def datetime_to_lectureyear(date):
     sept_1 = timezone.make_aware(timezone.datetime(2016, 9, 1))
-    if date.date() < sept_1.date():
+    if isinstance(date, timezone.datetime):
+        date = date.date()
+    if date < sept_1.date():
         return date.year - 1
     return date.year
 
