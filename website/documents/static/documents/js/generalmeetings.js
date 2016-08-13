a0ce02d7752335507a82d2a2bb42dc952465a35b
Joost Rijneveld <joost@joostrijneveld.nl>
2016-08-13 12:54:58 +0200

    Make all heights of ALVs equal
    This regressed from thalia/website!399

--- website/documents/static/documents/js/generalmeetings.js
+++ website/documents/static/documents/js/generalmeetings.js
@@ -1,8 +1,9 @@
 $(function() {
 
     function select_meeting(year) {
-        $("#alvcontainer > div").hide();
+        $("#meetingcontainer > div").hide();
         $("#meeting-"+year).show();
+        fix_heights($("#meeting-"+year));
     }
 
     $(".meeting-btn").on('click', function(e) {
@@ -13,4 +14,28 @@ $(function() {
         select_meeting($("#alvselect").val());
     })
 
+    function fix_heights(obj) {
+        var heights = new Array();
+
+        // // Loop to get all element heights
+        obj.find('.gw-go-col').each(function() {
+            // Need to let sizes be whatever they want so no overflow on resize
+            $(this).css('min-height', '0');
+            $(this).css('max-height', 'none');
+            $(this).css('height', 'auto');
+
+            // Then add size (no units) to array
+            heights.push($(this).height());
+        });
+
+        // // Find max height of all elements
+        var max = Math.max.apply( Math, heights );
+
+        // // Set all heights to max height
+        obj.find('.gw-go-col').each(function() {
+            $(this).css('height', max + 'px');
+        });
+    };
+
+    fix_heights($("#meetingcontainer > div"));
 });
