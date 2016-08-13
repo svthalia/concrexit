e2e7845296b28f5cccede48c8d26697489131d37
Thom Wiggers <thom@thomwiggers.nl>
2016-08-13 21:47:52 +0200

    Add name, email to add user form
    Closes #57

--- website/members/admin.py
+++ website/members/admin.py
@@ -4,6 +4,7 @@ This module registers admin pages for the models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 from django.utils.translation import ugettext_lazy as _
 
 from . import models
@@ -42,14 +43,27 @@ class MembershipTypeListFilter(admin.SimpleListFilter):
         return queryset.filter(pk__in=users)
 
 
+class UserCreationForm(BaseUserCreationForm):
+    class Meta(BaseUserCreationForm.Meta):
+        fields = ('username', 'first_name', 'last_name')
+
+
 class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+
     inlines = (MemberInline, MembershipInline)
     # FIXME include proper filter for expiration
     # https://docs.djangoproject.com/en/1.9/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_filter
     list_filter = (MembershipTypeListFilter,
                    'is_superuser',)
-    # FIXME use nicer form
-    # form = forms.AdminForm (base on ModelForm, reorder elements, etc).
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'username', 'email',
+                       'password1', 'password2'),
+        }),
+    )
 
 admin.site.register(models.BecomeAMemberDocument)
 
