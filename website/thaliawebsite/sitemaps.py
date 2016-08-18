d8be93565884e736ac8147260d4bc5459c168574
Thom Wiggers <thom@thomwiggers.nl>
2016-08-18 10:42:04 +0200

    Add become-a-member page
    Remove changefreq and priority

--- website/thaliawebsite/sitemaps.py
+++ website/thaliawebsite/sitemaps.py
@@ -3,11 +3,10 @@ from django.urls import reverse
 
 
 class StaticViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
 
     def items(self):
-        return ['index', 'become-active', 'sister-associations', 'contact']
+        return ['index', 'become-active', 'sister-associations',
+                'become-a-member', 'contact']
 
     def location(self, item):
         return reverse(item)
