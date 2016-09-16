11b911506e1c2339a851078354c8dc09199f8757
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2016-09-16 12:24:36 +0200

    Make merchandise multilingual, fix showing price in frontend and add a migration script

--- website/merchandise/models.py
+++ website/merchandise/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from utils.translation import MultilingualField, ModelTranslateMeta
 
 
-class MerchandiseItem(models.Model):
-    name = models.CharField(max_length=200)
+class MerchandiseItem(models.Model, metaclass=ModelTranslateMeta):
+    name = MultilingualField(models.CharField, max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    description = models.TextField()
+    description = MultilingualField(models.TextField)
     image = models.ImageField(upload_to='public/merchandise')
 
     def __str__(self):
