96e6d33fa4133bca8a87b1aaad7c2b5d5bd0ce7e
Luuk Scholten <info@luukscholten.com>
2016-09-20 21:45:22 +0200

    Implement duolingual iCal feed
    Todo: make old link redirect to the dutch ical feed

---
+++ website/events/feeds.py
@@ -0,0 +1,42 @@
+from django.urls import reverse
+from django.utils.translation import activate
+from django_ical.views import ICalFeed
+from events.models import Event
+from django.utils.translation import ugettext as _
+
+
+class EventFeed(ICalFeed):
+    def __init__(self, lang='en'):
+        super().__init__()
+        self.lang = lang
+
+    def product_id(self):
+        return '-//thalia.nu//EventCalendar//' + self.lang.upper()
+
+    def file_name(self):
+        return "thalia_{}.ics".format(self.lang)
+
+    def title(self):
+        activate(self.lang)
+        return _('Study Association Thalia event calendar')
+
+    def items(self):
+        return Event.objects.filter(published=True).order_by('-start')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    def item_start_datetime(self, item):
+        return item.start
+
+    def item_end_datetime(self, item):
+        return item.end
+
+    def item_link(self, item):
+        return reverse('events:event', kwargs={'event_id': item.id})
+
+    def item_location(self, item):
+        return "{} - {}".format(item.location, item.map_location)
--- website/events/urls.py
+++ website/events/urls.py
@@ -4,6 +4,7 @@ Events URL Configuration
 
 from django.conf.urls import url
 
+from events.feeds import EventFeed
 from . import views
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     url(r'admin/(?P<event_id>\d+)/export/$', views.export, name='export'),
     url(r'^(?P<event_id>\d+)$', views.event, name='event'),
     url(r'^$', views.index, name='index'),
+    url(r'^ical/nl.ics', EventFeed(lang='nl'), name='ical-nl'),
+    url(r'^ical/en.ics', EventFeed(), name='ical-en'),
 ]
