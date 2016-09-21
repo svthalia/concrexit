62776b302aa5176f5acae137b39ba8740cd718ba
WKuipers <wietse@kuiprs.nl>
2016-09-21 21:53:37 +0200

    Add album downloading
    Zip files are put in /tmp/ then served with sendfile

--- website/photos/urls.py
+++ website/photos/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^shared-download/(?P<slug>[-\w]+)/(?P<token>[a-zA-Z0-9]+)/(?P<path>.*)', views.shared_download, name='shared-download'),
     url(r'^shared-thumbnail/(?P<slug>[-\w]+)/(?P<token>[a-zA-Z0-9]+)/(?P<size_fit>\d+x\d+_[01])/(?P<path>.*)', views.shared_thumbnail, name='shared-thumbnail'),
     url(r'^(?P<slug>[-\w]+)/$', views.album, name='album'),
+    url(r'^(?P<slug>[-\w]+)/download$', views.album_download, name='album-download'),
     url(r'^(?P<slug>[-\w]+)/(?P<token>[a-zA-Z0-9]+)$', views.shared_album, name='shared_album'),
     url(r'^$', views.index, name='index'),
 ]
