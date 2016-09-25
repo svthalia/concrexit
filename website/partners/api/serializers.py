c058990bf0ecd826b114fb94fa36c0ad89515320
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2016-09-25 22:25:11 +0200

    Fix color and title of partner event

--- website/partners/api/serializers.py
+++ website/partners/api/serializers.py
@@ -10,11 +10,14 @@ class PartnerEventSerializer(CalenderJSSerializer):
     class Meta(CalenderJSSerializer.Meta):
         model = PartnerEvent
 
+    def _title(self, instance):
+        return "{} ({})".format(instance.title, instance.partner.name)
+
     def _background_color(self, instance):
         return '#E62272'
 
     def _text_color(self, instance):
-        return '#FFFFF'
+        return 'white'
 
     def _url(self, instance):
         return instance.url
