a699824bfe7ffff699f7f13444e6762d4e9a4494
Jelle Besseling <jelle@pingiun.com>
2016-09-28 18:52:55 +0200

    Add createfixtures command to manage.py to create fake user profiles
    Example usage:
    ./manage.py createfixtures -u 3
    This will create 3 fake users

---
+++ website/utils/management/commands/createfixtures.py
@@ -0,0 +1,79 @@
+import random
+import string
+import tempfile
+
+from django.contrib.auth.models import User
+
+from django.core.management.base import BaseCommand
+
+from django.core.files import File
+
+from members.models import Member, Membership
+from utils import identicon
+
+import factory
+from faker import Factory as FakerFactory
+faker = FakerFactory.create('nl_NL')
+
+
+class MemberFactory(factory.Factory):
+    class Meta:
+        model = Member
+
+    programme = random.choice(['computingscience', 'informationscience'])
+    student_number = factory.LazyAttribute(
+        lambda x: faker.bothify(text="s#######"))
+    starting_year = 2016
+
+    address_street = factory.LazyAttribute(lambda x: faker.street_address())
+    address_postal_code = factory.LazyAttribute(lambda x: faker.postcode())
+    address_city = factory.LazyAttribute(lambda x: faker.city())
+
+    phone_number = factory.LazyAttribute(lambda x: faker.phone_number())
+
+
+class Command(BaseCommand):
+    help = "Creates fake data to test the site with"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-u", "--user", type=int, help="The amount of fake users to add")
+
+    def handle(self, **options):
+        if options['user']:
+            print("Creating fake profiles for", options['user'], "users.")
+
+            for __ in range(options['user']):
+                try:
+                    fakeprofile = faker.profile()
+                    fakeprofile['password'] = ''.join(
+                        random.choice(string.ascii_uppercase + string.digits)
+                        for _ in range(16))
+                    user = User.objects.create_user(fakeprofile['username'],
+                                                    fakeprofile['mail'],
+                                                    fakeprofile['password'])
+                    user.first_name = fakeprofile['name'].split()[0]
+                    user.last_name = fakeprofile['name'].split()[1]
+
+                    member = MemberFactory()
+                    member.user_id = user.id
+                    member.birthday = fakeprofile['birthdate']
+                    member.website = fakeprofile['website'][0]
+
+                    code = sum([ord(x) for x in fakeprofile['username']])
+                    icon = identicon.render_identicon(code, 50)
+                    with tempfile.TemporaryFile() as tfile:
+                        icon.save(tfile, 'PNG')
+                        member.photo.save(fakeprofile['username'] + '.png',
+                                          File(tfile))
+
+                    membership = Membership()
+                    membership.user_id = user.id
+                    membership.type = 'member'
+                except Exception as e:
+                    raise e
+                else:
+                    user.save()
+                    member.save()
+                    membership.save()
+                    print("Created user with username", user.username)
