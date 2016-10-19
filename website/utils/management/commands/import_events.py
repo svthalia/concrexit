c256d7858b6b1cdd6119ccf5703416d82b47d63e
Joren Vrancken <jorenvrancken@gmail.com>
2016-10-19 22:49:26 +0200

    Fix tox warning

--- website/utils/management/commands/import_events.py
+++ website/utils/management/commands/import_events.py
@@ -2,7 +2,6 @@ import re
 import json
 import requests
 import pytz
-import sys
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.core.management.base import BaseCommand
