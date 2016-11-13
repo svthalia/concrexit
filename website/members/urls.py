8f8b0ff28bde8ccd057b1cf48dd48bd7432c6ad4
Joost Rijneveld <joost@joostrijneveld.nl>
2016-11-13 17:18:03 +0100

    Make url patterns more consistent

--- website/members/urls.py
+++ website/members/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
@@ -9,8 +9,6 @@ urlpatterns = [
         views.get_become_a_member_document,
         name='become-a-member-document'),
     url('^profile/(?P<pk>[0-9]*)$', views.profile, name='profile'),
-    url('^profile$', views.profile, name='profile'),
     url('^profile/edit/$', views.edit_profile, name='edit-profile'),
-    url('^account$', views.account, name='account'),
     url('^$', views.index, name='index'),
 ]
