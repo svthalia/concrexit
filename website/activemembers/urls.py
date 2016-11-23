ace1eb9dbecbc947122d04403dcfd710344135f6
Joost Rijneveld <joost@joostrijneveld.nl>
2016-11-23 21:35:02 +0100

    Make committee ID regex match ids >=10

--- website/activemembers/urls.py
+++ website/activemembers/urls.py
@@ -11,7 +11,7 @@ app_name = "activemembers"
 
 urlpatterns = [
     url(r'committees/$', views.committee_index, name='committees'),
-    url(r'^committees/(?P<id>\d)/$', views.committee_detail, name='committee'),
+    url(r'^committees/(?P<id>\d+)/$', views.committee_detail, name='committee'),
     url(r'^boards/$', views.board_index, name='boards'),
     url(r'^boards/(?P<id>\d+)?$', views.board_detail, name='board'),
 ]
