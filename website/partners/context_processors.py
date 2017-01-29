74071d81bab16b2e06b4370919b27f34df8853bc
Joost Rijneveld <joost@joostrijneveld.nl>
2017-01-29 15:34:14 +0100

    Update session-stored partners upon changes

--- website/partners/context_processors.py
+++ website/partners/context_processors.py
@@ -1,20 +1,25 @@
-from random import shuffle
+from random import sample
 
 from partners.models import Partner
 
 
 def showcased_partners(request):
-    if 'partner_sequence' not in request.session:
-        partner_ids = [p.id for p in Partner.objects.filter(is_active=True)]
-        shuffle(partner_ids)
-        request.session['partner_sequence'] = partner_ids
+    all_partners = Partner.objects.filter(is_active=True).order_by('id')
+    ids = [partner.id for partner in all_partners]
+
+    # check if partners have changed; if so, update sequence in session
+    if ('partner_sequence' not in request.session or
+            'partner_ids' not in request.session or
+            request.session['partner_ids'] != ids):
+        request.session['partner_ids'] = ids
+        request.session['partner_sequence'] = sample(ids, len(ids))
 
     sequence = request.session['partner_sequence']
     chosen, rest = sequence[:2], sequence[2:]
     request.session['partner_sequence'] = rest + chosen
 
     try:
-        partners = tuple(Partner.objects.get(id=id) for id in chosen)
+        partners = tuple(p for p in all_partners if p.id in chosen)
     except Partner.DoesNotExist:
         del request.session['partner_sequence']
         return showcased_partners(request)
