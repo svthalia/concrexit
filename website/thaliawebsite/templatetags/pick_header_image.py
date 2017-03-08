09e0a646c7dab47ce1d85d9ff9608bf40617eeaa
Milan van Stiphout <mstiphout@science.ru.nl>
2017-03-08 22:29:43 +0100

    Fixed errors.

--- website/thaliawebsite/templatetags/pick_header_image.py
+++ website/thaliawebsite/templatetags/pick_header_image.py
@@ -1,10 +1,11 @@
 from django import template
 from django.contrib.staticfiles.templatetags.staticfiles import static
-import os, random
+import os
+import random
 
 register = template.Library()
 banner_dir = static("/images/header_banners/")
-current_dir = os.listdir("." + banner_dir)
+current_dir = os.listdir("thaliawebsite/" + banner_dir)
 
 
 @register.simple_tag
