34ca924b9c3c97ca7e83d8e73d1ee5dfa7f30a16
Thom Wiggers <thom@thomwiggers.nl>
2017-03-11 11:39:12 +0100

    Fix regexes of urls
    The $ is important to note that it is a terminal

--- website/documents/urls.py
+++ website/documents/urls.py
@@ -5,18 +5,18 @@ from . import views
 app_name = "documents"
 
 urlpatterns = [
-    url('^miscellaneous/(?P<pk>[0-9]*)', views.get_miscellaneous_document,
+    url(r'^miscellaneous/(?P<pk>[0-9]+)/$', views.get_miscellaneous_document,
         name='miscellaneous-document'),
-    url('^(?P<document_type>(policy-document|annual-report|financial-report))/'
-        '(?P<year>[0-9]{4})',
+    url(r'^(?P<document_type>(policy-document|annual-report|financial-report))/'
+        r'(?P<year>[0-9]{4})/$',
         views.get_association_document,
         name='association-document'),
-    url('^general-meeting/(?P<pk>[0-9]*)/', include([
-        url('^minutes', views.get_general_meeting_minutes,
+    url(r'^general-meeting/(?P<pk>[0-9]+)/', include([
+        url(r'^minutes/$', views.get_general_meeting_minutes,
             name='minutes'),
         # TODO can we make this URL more pretty, using e.g. document seq nrs?
-        url('^(?P<document_pk>[0-9]*)', views.get_general_meeting_document,
+        url(r'^(?P<document_pk>[0-9]+)/$', views.get_general_meeting_document,
             name='general-meeting-document'),
     ])),
-    url('^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
