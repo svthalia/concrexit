0fd0b196dc760ff7bbe26adb2ca85e11a3f7de7d
Milan van Stiphout <mstiphout@science.ru.nl>
2017-03-15 20:39:43 +0100

    Fixed pep8 error: now unused import.

--- website/members/admin.py
+++ website/members/admin.py
@@ -6,7 +6,6 @@ import datetime
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext as t
 from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponse
 import csv
