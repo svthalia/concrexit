452b4f371a0395c3eab16486b37cefb15d2afb77
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2017-03-22 21:42:47 +0100

    Checked readme working and change source warning to BASH only

--- source_me.sh
+++ source_me.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pathname=$_
 
-if [[ "$pathname" = "$0" ]]; then
+if [[ -n "$BASH" && "$pathname" = "$0" ]]; then
     echo "Remember: you need to run me as 'source ./source_me.sh', not execute it!"
     return
 fi
