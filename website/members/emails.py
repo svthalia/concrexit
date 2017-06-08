03fc4f18aed43f4ea47e49e56e4a12682105964f
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2017-06-08 13:24:06 +0200

    Add basic membership email

---
+++ website/members/emails.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+
+from django.core import mail
+from django.template import loader
+from django.utils import translation
+from django.utils.translation import ugettext as _
+
+from members import models
+from thaliawebsite.settings import settings
+
+
+def send_membership_announcement(dry_run=False):
+    members = (models.Member.active_members
+               .filter(user__membership__until__isnull=True))
+
+    with mail.get_connection() as connection:
+        for member in members:
+            print("Send email to {} ({})".format(member.get_full_name(),
+                                                 member.user.email))
+            with translation.override(member.language):
+                email_body = loader.render_to_string(
+                    'members/email/membership_announcement.txt',
+                    {'name': member.get_full_name()})
+                mail.EmailMessage(
+                    _('Membership announcement'),
+                    email_body,
+                    settings.WEBSITE_FROM_ADDRESS,
+                    [member.user.email],
+                    bcc=[settings.BOARD_NOTIFICATION_ADDRESS],
+                    connection=connection
+                ).send()
+
+        if not dry_run:
+            mail.mail_managers(
+                _('Membership announcement sent'),
+                loader.render_to_string(
+                    'members/email/membership_announcement_notification.txt',
+                    {'members': members}),
+                connection=connection,
+            )
