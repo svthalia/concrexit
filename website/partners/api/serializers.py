376de684c1e1f800f1c967d1093798281f498c6f
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2017-09-30 14:28:15 +0200

    Add API endpoint for partner events

--- website/partners/api/serializers.py
+++ website/partners/api/serializers.py
@@ -1,8 +1,13 @@
+from html import unescape
+
+from django.utils.html import strip_tags
+from rest_framework import serializers
+
 from events.api.serializers import CalenderJSSerializer
-from partners.models import PartnerEvent
+from partners.models import PartnerEvent, Partner
 
 
-class PartnerEventSerializer(CalenderJSSerializer):
+class PartnerEventCalendarJSSerializer(CalenderJSSerializer):
     class Meta(CalenderJSSerializer.Meta):
         model = PartnerEvent
 
@@ -20,3 +25,22 @@ class PartnerEventSerializer(CalenderJSSerializer):
 
     def _target_blank(self, instance):
         return True
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partner
+        fields = ('pk', 'name', 'link', 'company_profile', 'address',
+                  'zip_code', 'city', 'logo')
+
+
+class PartnerEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PartnerEvent
+        fields = ('pk', 'title', 'description', 'start', 'end', 'location',
+                  'url')
+
+    description = serializers.SerializerMethodField('_description')
+
+    def _description(self, instance):
+        return unescape(strip_tags(instance.description))
--- website/partners/api/urls.py
+++ website/partners/api/urls.py
@@ -3,5 +3,6 @@ from rest_framework import routers
 from partners.api import viewsets
 
 router = routers.SimpleRouter()
+router.register(r'partners/events', viewsets.PartnerEventViewset)
 router.register(r'partners', viewsets.PartnerViewset)
 urlpatterns = router.urls
