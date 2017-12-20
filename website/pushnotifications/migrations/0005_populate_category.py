36790af2237c7fded0834ca8c417b9fa23856a49
Luko van der Maas <lukovandermaas@gmail.com>
2017-12-20 21:13:54 +0100

    revamed the categories to a model and added fields to device and message. Added filtering for pushnotifications

---
+++ website/pushnotifications/migrations/0005_populate_category.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def forwards_func(apps, schema_editor):
+    Category = apps.get_model("pushnotifications", "Category")
+    db_alias = schema_editor.connection.alias
+    Category.objects.using(db_alias).bulk_create([
+        Category(key="general", name_en="general", name_nl="algemeen"),
+        Category(key="pizza", name_en="pizza", name_nl="pizza"),
+        Category(key="event", name_en="event", name_nl="evenement"),
+        Category(key="newsletter", name_en="newsletter", name_nl="nieuwsbrief"),
+        Category(key="sponsor", name_en="sponsor", name_nl="sponsor"),
+        Category(key="photo", name_en="photo", name_nl="foto"),
+        Category(key="board", name_en="board", name_nl="bestuur"),
+    ])
+
+
+def reverse_func(apps, schema_editor):
+    Category = apps.get_model("pushnotifications", "Category")
+    db_alias = schema_editor.connection.alias
+    Category.objects.using(db_alias).all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pushnotifications', '0004_auto_20180221_1924'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards_func, reverse_func),
+    ]
