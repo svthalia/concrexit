ca44ec8a793bce3b34ff16de10083a8ad526ac0d
Thom Wiggers <thom@thomwiggers.nl>
2018-02-01 16:39:10 +0100

    Always pull newer versions of dependencies

--- release.sh
+++ release.sh
@@ -72,7 +72,7 @@ fi
 
 docker_tag="registry.gitlab.com/thaliawww/concrexit:$version"
 
-docker build -t "$docker_tag" .
+docker build --pull -t "$docker_tag" .
 
 if yesno "Do you want to push the container?"; then
     docker push "$docker_tag"
