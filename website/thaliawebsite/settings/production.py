103035bc33a16ad6b50fba97900721d413833083
Joren Vrancken <jorenvrancken@gmail.com>
2018-06-27 21:32:28 +0200

    Add PASSWORD to postgres config

--- website/thaliawebsite/settings/production.py
+++ website/thaliawebsite/settings/production.py
@@ -41,6 +41,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
         'NAME': os.environ.get('POSTGRES_DB'),
         'HOST': os.environ.get('DJANGO_POSTGRES_HOST'),
         'PORT': 5432,
