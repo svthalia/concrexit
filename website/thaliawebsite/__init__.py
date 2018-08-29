088842cd4f313c2ad2eefc413be6b14668fe8c19
Thom Wiggers <thom@thomwiggers.nl>
2018-08-29 20:31:24 +0200

    Remove unnecessary future imports

--- website/thaliawebsite/__init__.py
+++ website/thaliawebsite/__init__.py
@@ -4,8 +4,6 @@ The main module for the Thalia website.
 This module defines settings and the URI layout.
 We also handle some site-wide API stuff here.
 """
-from __future__ import absolute_import, unicode_literals
-
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
--- website/thaliawebsite/celery.py
+++ website/thaliawebsite/celery.py
@@ -1,5 +1,4 @@
 """Celery entry point"""
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
