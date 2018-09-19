1343e111ca58d28535c2f0dac8ddff91fb947a0a
Luko van der Maas <lukovandermaas@gmail.com>
2018-09-19 12:00:00 +0200

    fixed order of migrations

--- website/pushnotifications/migrations/0011_message_url.py
+++ website/pushnotifications/migrations/0011_message_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pushnotifications', '0008_scheduledmessage'),
+        ('pushnotifications', '0010_rename_partner_category'),
     ]
 
     operations = [
