cc07adb0788e468428e02638ffb900c0d313368f
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-10-01 12:53:33 +0200

    Move google maps creation to template tag and add parameters

---
+++ website/utils/templatetags/google_map_url.py
@@ -0,0 +1,10 @@
+from django import template
+
+from utils.snippets import create_google_maps_url
+
+register = template.Library()
+
+
+@register.simple_tag()
+def google_map_url(location, zoom=13, size='450x250'):
+    return create_google_maps_url(location, zoom, size)
