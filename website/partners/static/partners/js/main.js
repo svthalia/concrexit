1d661b7f2c495d7efd48b80da125415ae510801d
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-10-11 01:11:42 +0200

    Fix fancybox in partner page

--- website/partners/static/partners/js/main.js
+++ website/partners/static/partners/js/main.js
@@ -5,7 +5,12 @@ $(function () {
                 type: 'float'
             }
         },
-        padding: 0
+		padding: 0,
+        tpl: {
+		    closeBtn: '<a title="Close" class="btn btn-primary fancybox-close" href="javascript:;"><i class="fas fa-times"></i></a>',
+            next: '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span class="btn btn-primary"><i class="fas fa-arrow-right"></i></span></a>',
+            prev: '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span class="btn btn-primary"><i class="fas fa-arrow-left"></i></span></a>'
+        }
     });
 
     var windowhash = window.location.hash;
