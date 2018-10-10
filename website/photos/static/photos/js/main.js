585efd2884d8c5981483b73b9eb5d6715286ca18
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-10-10 20:56:05 +0200

    Fix fancybox styling

--- website/photos/static/photos/js/main.js
+++ website/photos/static/photos/js/main.js
@@ -8,9 +8,14 @@ $(function() {
 		afterShow: function() {
 			var downloadUrl = $(this.element).attr('data-download');
 			if(downloadUrl != undefined) {
-				$('<a class="fancybox-download" href="' + downloadUrl + '"></a>').appendTo(this.outer);
+				$('<a class="btn btn-primary fancybox-download" href="' + downloadUrl + '"><i class="fas fa-download"></i></a>').appendTo(this.outer);
 			}
 		},
-		padding: 0
+		padding: 0,
+        tpl: {
+		    closeBtn: '<a title="Close" class="btn btn-primary fancybox-close" href="javascript:;"><i class="fas fa-times"></i></a>',
+            next: '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span class="btn btn-primary"><i class="fas fa-arrow-right"></i></span></a>',
+            prev: '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span class="btn btn-primary"><i class="fas fa-arrow-left"></i></span></a>'
+        }
 	});
 });
