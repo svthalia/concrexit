1d1bb7723b3e2572026959437ec4ad359bb2e02a
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-10-14 00:50:46 +0200

    Migrate thabloid to new template

---
+++ website/thabloid/templatetags/thabloid_cards.py
@@ -0,0 +1,36 @@
+from django import template
+from django.urls import reverse
+
+from thaliawebsite.templatetags.grid_item import grid_item
+from utils.templatetags.thumbnail import thumbnail
+
+register = template.Library()
+
+
+@register.inclusion_tag('includes/grid_item.html')
+def thabloid_card(year, thabloid):
+    class_name = 'older'
+    if thabloid.year - 3 < year:
+        class_name = 'year-{}'.format(thabloid.year)
+
+    view_url = reverse('thabloid:pages', args=[thabloid.year, thabloid.issue])
+    buttons = (
+        '<div class="text-center mt-2">'
+        '<a href="{}" class="btn btn-secondary d-inline-flex open mr-1">'
+        '<i class="fas fa-book-open"></i>'
+        '</a>'
+        '<a href="{}" class="btn btn-secondary d-inline-flex download ml-1">'
+        '<i class="fas fa-download"></i>'
+        '</a>'
+        '</div>'
+    ).format(view_url, thabloid.file.url)
+
+    return grid_item(
+        title='{}-{}, #{}'.format(
+            thabloid.year, thabloid.year + 1, thabloid.issue
+        ),
+        meta_text=buttons,
+        url=None,
+        image_url=thumbnail(thabloid.cover, '255x360'),
+        class_name='thabloid-card mix {}'.format(class_name),
+    )
