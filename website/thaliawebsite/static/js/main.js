18748d8f36c29c7e637bc4ea39d936afdb8b6fd3
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-10-18 15:54:01 +0200

    Change language switcher to use form in base.html

--- website/thaliawebsite/static/js/main.js
+++ website/thaliawebsite/static/js/main.js
@@ -3,35 +3,7 @@ $(function () {
 
     $("#language-switcher").click(function(e) {
         e.preventDefault();
-        var selector = $("#language-switcher");
-        var next = selector.data('next');
-        var lang = selector.data('lang');
-        var href = selector.data('href');
-
-        var form = $('<form></form>');
-        form.attr("method", "post");
-        form.attr("action", href);
-
-        var langField = $('<input/>');
-        langField.attr("type", "hidden");
-        langField.attr("name", "language");
-        langField.attr("value", lang);
-        form.append(langField);
-
-        var nextField = $('<input/>');
-        nextField.attr("type", "hidden");
-        nextField.attr("name", "next");
-        nextField.attr("value", next);
-        form.append(nextField);
-
-        var csrfField = $('<input/>');
-        csrfField.attr("type", "hidden");
-        csrfField.attr("name", 'csrfmiddlewaretoken');
-        csrfField.attr("value", Cookies.get('csrftoken'));
-        form.append(csrfField);
-
-        $(document.body).append(form);
-        form.submit();
+        $("#change-language-form").submit();
         return false;
     });
 });
