8b0266dc14c7ebced0b9af3acb553b77c23a7e5f
Luko van der Maas <lukovandermaas@gmail.com>
2018-11-14 14:14:20 +0100

    added all statistics back

--- website/members/static/members/js/statistics.js
+++ website/members/static/members/js/statistics.js
@@ -5,7 +5,54 @@ $(function () {
     var pizzaOrders = statistics.total_pizza_orders;
     var currentPizzaOrders = statistics.current_pizza_orders;
 
-    new Chart($('#membersTypeChart'), {
+    Chart.defaults.global.aspectRatio = 1;
+    Chart.defaults.global.legend.display = false;
+    Chart.defaults.global.title.display = true;
+
+    var pluginOptions = {
+        labels: {
+            render: 'label',
+            fontColor: '#000000',
+            arc: false,
+            position: 'outside',
+            fontsize: 16,
+            fontstyle: 'bold',
+            fontFamily: '"Open Sans"'
+        }
+    };
+
+    new Chart($('#members-type-chart'), {
+        type: 'pie',
+        data: {
+            labels:
+                [
+                    gettext('Members'),
+                    gettext('Supporters'),
+                    gettext('Honorary Members'),
+                ],
+            datasets: [{
+                backgroundColor: [
+                    '#AE0046',
+                    '#E62272',
+                    '#E6478A',
+                ],
+                data:
+                    [
+                        memberTypeDistribution.member,
+                        memberTypeDistribution.supporter,
+                        memberTypeDistribution.honorary
+                    ],
+            }]
+        },
+        options: {
+            title: {
+                text: gettext('Members per member type'),
+            },
+            plugins: pluginOptions,
+        }
+    });
+
+    new Chart($('#total-year-chart'), {
         type: 'pie',
         data: {
             labels:
@@ -38,22 +85,172 @@ $(function () {
             }]
         },
         options: {
-            aspectRatio: 1,
-            legend: {
-                display: false
-            },
             title: {
-                display: true,
-                text: gettext("Total number of (honary) members and benefactors per cohort"),
+                text: gettext("Total number of (honary) members and supporters per cohort"),
             },
-            plugins: {
-                labels: {
-                    render: 'label',
-                    fontColor: '#fff',
-                    arc: true,
-                    position: 'border',
-                }
-            }
+            plugins: pluginOptions,
         }
     });
+
+    new Chart($('#members-year-chart'), {
+        type: 'pie',
+        data: {
+            labels:
+                [
+                    cohortSizes[0].cohort,
+                    cohortSizes[1].cohort,
+                    cohortSizes[2].cohort,
+                    cohortSizes[3].cohort,
+                    cohortSizes[4].cohort,
+                    cohortSizes[5].cohort
+                ],
+            datasets: [{
+                backgroundColor: [
+                    '#AE0046',
+                    '#E62272',
+                    '#E6478A',
+                    '#CC2482',
+                    '#8E1056',
+                    '#DC3472',
+                ],
+                data:
+                    [
+                        cohortSizes[0].member,
+                        cohortSizes[1].member,
+                        cohortSizes[2].member,
+                        cohortSizes[3].member,
+                        cohortSizes[4].member,
+                        cohortSizes[5].member,
+                    ],
+            }]
+        },
+        options: {
+            title: {
+                text: gettext("Members per cohort (honorary excluded)"),
+            },
+            plugins: pluginOptions,
+        }
+    });
+
+    new Chart($('#benefactors-year-chart'), {
+        type: 'pie',
+        data: {
+            labels:
+                [
+                    cohortSizes[0].cohort,
+                    cohortSizes[1].cohort,
+                    cohortSizes[2].cohort,
+                    cohortSizes[3].cohort,
+                    cohortSizes[4].cohort,
+                    cohortSizes[5].cohort
+                ],
+            datasets: [{
+                backgroundColor: [
+                    '#AE0046',
+                    '#E62272',
+                    '#E6478A',
+                    '#CC2482',
+                    '#8E1056',
+                    '#DC3472',
+                ],
+                data:
+                    [
+                        cohortSizes[0].supporter,
+                        cohortSizes[1].supporter,
+                        cohortSizes[2].supporter,
+                        cohortSizes[3].supporter,
+                        cohortSizes[4].supporter,
+                        cohortSizes[5].supporter,
+                    ],
+            }]
+        },
+        options: {
+            title: {
+                text: gettext("Supporters per cohort"),
+            },
+            plugins: pluginOptions,
+        }
+    });
+
+    new Chart($('#pizza-total-type-chart'), {
+        type: 'pie',
+        data: {
+            labels:
+                [
+                    pizzaOrders[0].name,
+                    pizzaOrders[1].name,
+                    pizzaOrders[2].name,
+                    pizzaOrders[3].name,
+                    pizzaOrders[4].name,
+                    pizzaOrders[5].name
+                ],
+            datasets: [{
+                backgroundColor: [
+                    '#AE0046',
+                    '#E62272',
+                    '#E6478A',
+                    '#CC2482',
+                    '#8E1056',
+                    '#DC3472',
+                ],
+                data:
+                    [
+                        pizzaOrders[0].total,
+                        pizzaOrders[1].total,
+                        pizzaOrders[2].total,
+                        pizzaOrders[3].total,
+                        pizzaOrders[4].total,
+                        pizzaOrders[5].total
+                    ],
+            }]
+        },
+        options: {
+            title: {
+                text: gettext("Total pizza orders of type"),
+            },
+            plugins: pluginOptions,
+        }
+    });
+
+    if (current_pizza_orders != null) {
+        new Chart($('#pizza-current-type-chart'), {
+            type: 'pie',
+            data: {
+                labels:
+                    [
+                        currentPizzaOrders[0].name,
+                        currentPizzaOrders[1].name,
+                        currentPizzaOrders[2].name,
+                        currentPizzaOrders[3].name,
+                        currentPizzaOrders[4].name,
+                        currentPizzaOrders[5].name
+                    ],
+                datasets: [{
+                    backgroundColor: [
+                        '#AE0046',
+                        '#E62272',
+                        '#E6478A',
+                        '#CC2482',
+                        '#8E1056',
+                        '#DC3472',
+                    ],
+                    data:
+                        [
+                            currentPizzaOrders[0].total,
+                            currentPizzaOrders[1].total,
+                            currentPizzaOrders[2].total,
+                            currentPizzaOrders[3].total,
+                            currentPizzaOrders[4].total,
+                            currentPizzaOrders[5].total
+                        ],
+                }]
+            },
+            options: {
+                title: {
+                    text: gettext("Current pizza orders of type"),
+                },
+                plugins: pluginOptions,
+            }
+        });
+    }
 });
