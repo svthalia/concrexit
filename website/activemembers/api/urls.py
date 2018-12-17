977e925442ff0581ea3f3bbaf35d099f57026352
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-12-17 15:32:20 +0100

    Add admin group to NextCloud api

--- website/activemembers/api/urls.py
+++ website/activemembers/api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from activemembers.api import views
 
 urlpatterns = [
-    path('activemembers/nextcloud/users', views.NextCloudUsersView.as_view()),
-    path('activemembers/nextcloud/groups', views.NextCloudGroupsView.as_view()),
+    path('activemembers/nextcloud/users/',
+         views.NextCloudUsersView.as_view()),
+    path('activemembers/nextcloud/groups/',
+         views.NextCloudGroupsView.as_view()),
 ]
--- website/activemembers/api/views.py
+++ website/activemembers/api/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import Permission
+from django.db.models import Q
+from django.utils import timezone
 from rest_framework.generics import ListAPIView
 
 from activemembers.api.permissions import NextCloudPermission
@@ -11,14 +14,48 @@ from members.models import Member
 
 class NextCloudUsersView(ListAPIView):
     permission_classes = [NextCloudPermission]
-    queryset = Member.current_members.filter(
-        pk__in=MemberGroupMembership.active_objects.values_list(
-            'member_id', flat=True)
-    )
+    queryset = Member.current_members.all()
     serializer_class = NextCloudMemberSerializer
+
+    def get_queryset(self):
+        perm = Permission.objects.get(content_type__app_label='auth',
+                                      codename='nextcloud_admin')
+        return super().get_queryset().filter(
+            Q(pk__in=MemberGroupMembership.active_objects.values_list(
+                'member_id', flat=True)) |
+            Q(is_superuser=True) |
+            Q(groups__permissions=perm) |
+            Q(user_permissions=perm) |
+            (Q(membergroup__permissions=perm) &
+             (Q(membergroupmembership__until=None) |
+              Q(membergroupmembership__until__gte=timezone.now())))
+        ).distinct()
 
 
 class NextCloudGroupsView(ListAPIView):
     permission_classes = [NextCloudPermission]
     queryset = MemberGroup.objects.all()
     serializer_class = NextCloudGroupSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        perm = Permission.objects.get(content_type__app_label='auth',
+                                      codename='nextcloud_admin')
+        users = Member.current_members.filter(
+            Q(is_superuser=True) |
+            Q(groups__permissions=perm) |
+            Q(user_permissions=perm) |
+            (Q(membergroup__permissions=perm) &
+             (Q(membergroupmembership__until=None) |
+              Q(membergroupmembership__until__gte=timezone.now())))
+        ).distinct().values_list('username', flat=True)
+
+        if users.count() > 0:
+            response.data = list(response.data) + [
+                {
+                    'pk': -1,
+                    'name': 'admin',
+                    'members': users
+                }
+            ]
+        return response
