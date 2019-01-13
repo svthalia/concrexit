3d8c5d22308d50d14027cbc41bb45e9813726377
Joren Vrancken <jorenvrancken@gmail.com>
2019-01-13 21:41:03 +0100

    Remove XFrameOptionsMiddleware
    The XFrameOptionsMiddleware middleware sets the `X-Frame-Options` header, which we already do in our `nginx` instance.

--- website/thaliawebsite/settings/settings.py
+++ website/thaliawebsite/settings/settings.py
@@ -99,7 +99,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     # Our middleware
     'members.middleware.MemberMiddleware',
