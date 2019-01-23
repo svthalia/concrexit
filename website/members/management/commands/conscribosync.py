3eb3e02d8ce889298969190a5b8ef572652935dc
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2019-01-23 20:53:22 +0100

    Fix initials reference

--- website/members/management/commands/conscribosync.py
+++ website/members/management/commands/conscribosync.py
@@ -56,7 +56,7 @@ class Command(BaseCommand):
                     'plaats': profile.address_city,
                     'land': 'Nederland',
                     'bankrekeningnummer': {
-                            'name': f'${member.initials} ${member.last_name}',
+                            'name': f'${profile.initials} ${member.last_name}',
                             'bic': '',
                             'iban': profile.bank_account,
                         },
