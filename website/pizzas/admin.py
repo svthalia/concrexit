bfc757cffce264ea478adc1bc881ac06c9cf27d2
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2019-02-13 21:41:56 +0100

    Only allow changing pizza orders if organiser of event

--- website/pizzas/admin.py
+++ website/pizzas/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 
+from pizzas import services
 from .models import Order, PizzaEvent, Product
 from events.models import Event
 
@@ -35,3 +37,29 @@ class PizzaEventAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('pizza_event', 'member_name', 'product', 'paid')
+
+    def save_model(self, request, obj, form, change):
+        if not services.is_organiser(request.member, obj.pizza_event):
+            raise PermissionDenied
+        return super().save_model(request, obj, form, change)
+
+    def has_view_permission(self, request, order=None):
+        """Only give view permission if the user is an organiser"""
+        if (order is not None and
+                not services.is_organiser(request.member, order.pizza_event)):
+            return False
+        return super().has_view_permission(request, order)
+
+    def has_change_permission(self, request, order=None):
+        """Only give change permission if the user is an organiser"""
+        if (order is not None and
+                not services.is_organiser(request.member, order.pizza_event)):
+            return False
+        return super().has_change_permission(request, order)
+
+    def has_delete_permission(self, request, order=None):
+        """Only give delete permission if the user is an organiser"""
+        if (order is not None and
+                not services.is_organiser(request.member, order.pizza_event)):
+            return False
+        return super().has_delete_permission(request, order)
