8632520d5a7823aa1cae63fe672ad3fb30d9a9fc
Michiel Kraan <m.kraan@student.science.ru.nl>
2019-03-27 20:38:46 +0100

    Write newsletter html to file on sending

---
+++ website/newsletters/management/commands/createnewsletterhtml.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.http import HttpRequest
+
+from newsletters import emails, models
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--include-unsent',
+            action='store_true',
+            dest='include-unsent',
+            default=False,
+            help="Include newsletters that haven't been sent yet"
+        )
+        parser.add_argument(
+            'server-name',
+            help='The server name for the request '
+                 'to generate the html (typically thalia.nu)'
+        )
+        parser.add_argument(
+            'server-port',
+            type=int,
+            help='The server port for the request '
+                 'to generate the html (typically 80)'
+        )
+
+    def handle(self, *args, **options):
+        request = HttpRequest()
+        request.META['SERVER_NAME'] = options['server-name']
+        request.META['SERVER_PORT'] = options['server-port']
+        for n in models.Newsletter.objects.all():
+            if n.sent or options['include-unsent']:
+                emails.save_to_disk(n, request)
