f95b7363365cad553f5164c11027812e93412295
Luko van der Maas <l.vandermaas@student.science.ru.nl>
2019-05-22 20:15:54 +0200

    Fix code style and add translations

--- website/events/admin_views.py
+++ website/events/admin_views.py
@@ -5,7 +5,7 @@ from django.contrib.admin import helpers
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
