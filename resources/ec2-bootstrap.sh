f358ddde32d1a0f7ffc09d299106dcd313543af2
Joren Vrancken <jorenvrancken@gmail.com>
2020-01-22 12:47:25 +0100

    Create review EC2 instances in a private subnet and use *private.review.technicie.nl instead of *.reviewhost.

--- resources/ec2-bootstrap.sh
+++ resources/ec2-bootstrap.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # From https://docs.docker.com/install/linux/docker-ce/ubuntu/
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 
