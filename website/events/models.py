b2dc078a8b013d3207271add9508d6d984ecd509
Mario Tsatsev <mario.tsatsev@student.ru.nl>
2020-02-26 20:31:18 +0100

    warn_the_organized_of_change_in_the_number_of_participants

--- website/events/models.py
+++ website/events/models.py
@@ -477,7 +477,7 @@ class Event(models.Model, metaclass=ModelTranslateMeta):
                 delete_collector.add([self.start_reminder])
                 self.start_reminder = None
 
-            if self._max_participants != self.max_participants:
+        if self._max_participants != self.max_participants:
                 message.warning(request,f"The maximum number of participants has changed, please inform those who might be affected by it")
 
         super().save()
