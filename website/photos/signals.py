91f6795a507ceab6ac1e3d864fe09fb1da2c5833
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2020-03-28 12:48:22 +0100

    Remove photo files on delete of model

---
+++ website/photos/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import pre_delete
+
+from utils.models.signals import suspendingreceiver
+
+
+@suspendingreceiver(
+    pre_delete, sender="photos.Photo", dispatch_uid="photos_photo_delete"
+)
+def pre_photo_delete(sender, instance, **kwargs):
+    """Remove main photo file on deletion"""
+    instance.file.delete()
