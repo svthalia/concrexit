92a5f8d67e0d762010e9e6971bc8c52c9b012c75
Sébastiaan <1799914+se-bastiaan@users.noreply.github.com>
2020-08-03 09:43:19 +0200

    Remove dependency of pizzas app on Payment.NONE in API (#1172)

--- website/payments/api/fields.py
+++ website/payments/api/fields.py
@@ -1,10 +1,15 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
-
-from payments.models import Payment
 
 
 class PaymentTypeField(serializers.ChoiceField):
+    NO_PAYMENT = "no_payment"
+
+    def __init__(self, choices, **kwargs):
+        choices = choices + (self.NO_PAYMENT, _("No payment"))
+        super().__init__(choices, **kwargs)
+
     def get_attribute(self, instance):
         if not instance.payment:
-            return Payment.NONE
+            return self.NO_PAYMENT
         return super().get_attribute(instance)
