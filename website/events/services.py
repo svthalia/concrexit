857a0d00e94d283f4a9e7179a84dc8ffae93b2b9
Sébastiaan <se_bastiaan@outlook.com>
2020-09-04 16:06:55 +0200

    Do not create payment when payment is NONE (#1204)

--- website/events/services.py
+++ website/events/services.py
@@ -293,11 +293,9 @@ def update_registration_by_organiser(registration, member, data):
         raise RegistrationError(_("You are not allowed to update this registration."))
 
     if "payment" in data:
-        if (
-            data["payment"]["type"] == PaymentTypeField.NO_PAYMENT
-            and registration.payment is not None
-        ):
-            delete_payment(registration)
+        if data["payment"]["type"] == PaymentTypeField.NO_PAYMENT:
+            if registration.payment is not None:
+                delete_payment(registration)
         else:
             registration.payment = create_payment(
                 payable=registration,
