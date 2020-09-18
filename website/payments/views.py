1f507a01fc225fe28dcd844d46854eb7c4f19bd8
Job Doesburg <job.doesburg@gmail.com>
2020-09-18 23:30:24 +0200

    Use url

--- website/payments/views.py
+++ website/payments/views.py
@@ -20,7 +20,6 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, FormView
 
-from members.decorators import membership_required
 from payments import services
 from payments.exceptions import PaymentError
 from payments.forms import BankAccountForm, PaymentCreateForm
