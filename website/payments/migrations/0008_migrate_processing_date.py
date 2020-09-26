cb5ee019c94cff7123527814181a01e5ebda43ff
Sébastiaan <se_bastiaan@outlook.com>
2020-09-26 13:41:50 +0200

    Fix payments remove processing date migration (#1251)

--- website/payments/migrations/0008_migrate_processing_date.py
+++ website/payments/migrations/0008_migrate_processing_date.py
@@ -8,7 +8,24 @@ def set_created_at(apps, schema_editor):
         if payment.processing_date:
             payment.created_at = payment.processing_date
         else:
+            if hasattr(payment, 'pizzas_order'):
+                order = payment.pizzas_order
+                order.payment = None
+                order.save()
+
+            if hasattr(payment, 'registrations_entry'):
+                entry = payment.registrations_entry
+                entry.payment = None
+                entry.save()
+
             payment.delete()
+
+
+def set_processed_date(apps, schema_editor):
+    """Revert sets the processing date to the created_at value."""
+    Payment = apps.get_model('payments', 'Payment')
+    for payment in Payment.objects.all():
+        payment.processing_date = payment.created_at
 
 
 class Migration(migrations.Migration):
@@ -18,5 +35,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(set_created_at)
+        migrations.RunPython(set_created_at, set_processed_date)
     ]
