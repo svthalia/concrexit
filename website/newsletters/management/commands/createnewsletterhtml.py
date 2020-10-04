68c711791a8ef6410c8094061d3d65919f9a62de
Simcha van Collem <simcha@vancollem.nl>
2020-10-04 13:38:13 +0200

    Removed unneeded arguments from createnewsletterhtml command

--- website/newsletters/management/commands/createnewsletterhtml.py
+++ website/newsletters/management/commands/createnewsletterhtml.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.http import HttpRequest
 
 from newsletters import models, services
 
@@ -12,17 +11,6 @@ class Command(BaseCommand):
             dest="include-unsent",
             default=False,
             help="Include newsletters that haven't been sent yet",
-        )
-        parser.add_argument(
-            "server-name",
-            help="The server name for the request "
-            "to generate the html (typically thalia.nu)",
-        )
-        parser.add_argument(
-            "server-port",
-            type=int,
-            help="The server port for the request "
-            "to generate the html (typically 80)",
         )
 
     def handle(self, *args, **options):
