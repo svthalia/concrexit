2bf6e5d63becc78aab0e6ea87b41001b76cecc47
Jelle Besseling <jelle@pingiun.com>
2020-10-22 15:16:49 +0200

    Refactor logging and make logfile optional

--- website/manage.py
+++ website/manage.py
@@ -8,6 +8,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thaliawebsite.settings")
+    os.environ.setdefault("ENABLE_LOGFILE", "0")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
