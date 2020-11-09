cdf61c942235b84481b31ee51d97357c89615fcb
Sébastiaan <se_bastiaan@outlook.com>
2020-11-09 20:30:33 +0100

    Fix registration openapi spec (#1384)

--- website/thaliawebsite/api/openapi.py
+++ website/thaliawebsite/api/openapi.py
@@ -1,6 +1,7 @@
 from oauth2_provider.scopes import get_scopes_backend
 from rest_framework.reverse import reverse
 from rest_framework.schemas.openapi import SchemaGenerator, AutoSchema
+from rest_framework.schemas.utils import is_list_view
 
 
 class OAuthSchemaGenerator(SchemaGenerator):
@@ -26,5 +27,22 @@ class OAuthAutoSchema(AutoSchema):
     def get_operation(self, path, method):
         operation = super().get_operation(path, method)
         if self.view and hasattr(self.view, "required_scopes"):
-            operation["security"] = {"oauth2": self.view.required_scopes}
+            operation["security"] = [{"oauth2": self.view.required_scopes}]
+        else:
+            operation["security"] = [{"oauth2": ["read", "write"]}]
         return operation
+
+    def get_operation_id(self, path, method):
+        method_name = getattr(self.view, "action", method.lower())
+        if is_list_view(path, method, self.view):
+            action = "list"
+        elif method_name not in self.method_mapping:
+            action = self._to_camel_case(
+                f"{self.method_mapping[method.lower()]}_{method_name}"
+            )
+        else:
+            action = self.method_mapping[method.lower()]
+
+        name = self.get_operation_id_base(path, method, action)
+
+        return action + name
