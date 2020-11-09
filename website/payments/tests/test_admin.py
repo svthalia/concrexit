a2aefa48e11e44eb61466cb4288e14c154f917ec
Luko van der Maas <me@luko.dev>
2020-11-09 23:10:12 +0100

    fix test

--- website/payments/tests/test_admin.py
+++ website/payments/tests/test_admin.py
@@ -889,7 +889,7 @@ class PaymentUserAdminTest(TestCase):
 
     @mock.patch("payments.models.PaymentUser.tpay_balance", new_callable=PropertyMock)
     @mock.patch("payments.models.PaymentUser.tpay_enabled", new_callable=PropertyMock)
-    def test_get_tpay_balance(self, tpay_balance, tpay_enabled):
+    def test_get_tpay_balance(self, tpay_enabled, tpay_balance):
         tpay_balance.return_value = Decimal(-10)
         tpay_enabled.return_value = True
         self.assertEquals(self.admin.get_tpay_balance(self.user), "€ -10.00")
