a03eb9415db0b9f955180652101a790488698490
Job Doesburg <job.doesburg@gmail.com>
2020-11-09 21:38:33 +0100

    Do not show Thalia Pay buttons if master setting disabled (#1390)

--- website/payments/tests/test_signals.py
+++ website/payments/tests/test_signals.py
@@ -7,7 +7,9 @@ from payments.models import PaymentUser
 
 
 class SignalsTest(TestCase):
-    @override_settings(THALIA_PAY_FOR_NEW_MEMBERS=True)
+    @override_settings(
+        THALIA_PAY_FOR_NEW_MEMBERS=True, THALIA_PAY_ENABLED_PAYMENT_METHOD=True
+    )
     def test_give_new_users_tpay_permissions(self):
         member = Member.objects.create(username="test", first_name="", last_name="")
         Profile.objects.create(
