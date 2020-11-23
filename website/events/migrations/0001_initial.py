d497faaf0030b603c98dddc53826076618175e30
Mario <>
2020-11-23 21:00:38 +0100

    LogicalAndandBrackets

--- website/events/migrations/0001_initial.py
+++ website/events/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+class Migration(migrations.Migration): 
 
     initial = True
 
