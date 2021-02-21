1163b7eb484cc58fd2ac46cf926302e8a5c6af09
Sébastiaan <se_bastiaan@outlook.com>
2021-02-21 22:14:03 +0100

    Fix API v1 global pagination

--- website/thaliawebsite/api/pagination.py
+++ website/thaliawebsite/api/pagination.py
@@ -6,7 +6,7 @@ class APIv2LimitOffsetPagination(LimitOffsetPagination):
     """Pagination class that uses LimitOffsetPagination and sets the default value for the pagination size to None for API v1."""
 
     def get_limit(self, request):
-        if self.limit_query_param:
+        if self.limit_query_param in request.query_params:
             return super().get_limit(request)
 
         if request.version == "v1":
