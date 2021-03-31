d8f519457930fced4282e2040123b22041f3ab20
Sébastiaan <se_bastiaan@outlook.com>
2021-03-31 10:22:18 +0200

    Add current member API endpoints (#1589)

--- website/members/api/v2/urls.py
+++ website/members/api/v2/urls.py
@@ -1,9 +1,10 @@
 """Members app API v2 urls."""
 from django.urls import path
 
-from members.api.v2.views import MemberListView, MemberDetailView
+from members.api.v2.views import MemberListView, MemberDetailView, MemberCurrentView
 
 urlpatterns = [
     path("members/", MemberListView.as_view(), name="member-list"),
     path("members/<int:pk>/", MemberDetailView.as_view(), name="member-detail",),
+    path("members/me/", MemberCurrentView.as_view(), name="member-current",),
 ]
