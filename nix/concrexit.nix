403692805b5bdf464275b0301c066d92b04a052f
Jelle Besseling <jelle@pingiun.com>
2021-04-26 20:34:37 +0200

    Replace python magic with nixpkgs version

--- nix/concrexit.nix
+++ nix/concrexit.nix
@@ -13,44 +13,9 @@ let
     # The overrides we do here are patches copied from nixpkgs, some patches are also included
     # in poetry2nix but for Pillow they don't work well enough and python-magic isn't included
     overrides = poetry2nix.overrides.withDefaults (
-      self: super: {
-        pillow = super.pillow.overridePythonAttrs (
-          old: with pkgs; {
-            preConfigure =
-              let
-                libinclude' = pkg: ''"${pkg.out}/lib", "${pkg.out}/include"'';
-                libinclude = pkg: ''"${pkg.out}/lib", "${pkg.dev}/include"'';
-              in
-              ''
-                sed -i "setup.py" \
-                    -e 's|^FREETYPE_ROOT =.*$|FREETYPE_ROOT = ${libinclude freetype}|g ;
-                        s|^JPEG_ROOT =.*$|JPEG_ROOT = ${libinclude libjpeg}|g ;
-                        s|^JPEG2K_ROOT =.*$|JPEG2K_ROOT = ${libinclude openjpeg}|g ;
-                        s|^IMAGEQUANT_ROOT =.*$|IMAGEQUANT_ROOT = ${libinclude' libimagequant}|g ;
-                        s|^ZLIB_ROOT =.*$|ZLIB_ROOT = ${libinclude zlib}|g ;
-                        s|^LCMS_ROOT =.*$|LCMS_ROOT = ${libinclude lcms2}|g ;
-                        s|^TIFF_ROOT =.*$|TIFF_ROOT = ${libinclude libtiff}|g ;
-                        s|^TCL_ROOT=.*$|TCL_ROOT = ${libinclude' tcl}|g ;
-                        s|self\.disable_platform_guessing = None|self.disable_platform_guessing = True|g ;'
-                export LDFLAGS="-L${libwebp}/lib"
-                export CFLAGS="-I${libwebp}/include"
-              ''
-              # Remove impurities
-              + stdenv.lib.optionalString stdenv.isDarwin ''
-                substituteInPlace setup.py \
-                  --replace '"/Library/Frameworks",' "" \
-                  --replace '"/System/Library/Frameworks"' ""
-              '';
-            nativeBuildInputs = [ pkgconfig ] ++ old.nativeBuildInputs;
-            propagatedBuildInputs = [ self.olefile ];
-            buildInputs = [ freetype libjpeg openjpeg zlib libtiff libwebp tcl lcms2 ] ++ old.buildInputs;
-          }
-        );
-        python-magic = super.python-magic.overridePythonAttrs (_old: {
-          postPatch = ''
-            substituteInPlace magic.py --replace "ctypes.util.find_library('magic')" "'${file}/lib/libmagic${stdenv.hostPlatform.extensions.sharedLibrary}'"
-          '';
-        });
+      _self: _super: {
+        pillow = pkgs.python3.pkgs.pillow;
+        python-magic = pkgs.python3.pkgs.python_magic;
         # We don't install uswgi from pypi but instead use the nixpkgs version
         uwsgi = { };
       }
