1366f58caa0fb6041249d6f2dc74de99094678ee
Sébastiaan <se_bastiaan@outlook.com>
2021-05-10 21:21:46 +0200

    Fix sales admin member check (#1664)

--- website/sales/admin/order_admin.py
+++ website/sales/admin/order_admin.py
@@ -223,7 +223,7 @@ class OrderAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
 
-        if not (
+        if request.member and not (
             request.member.is_superuser
             or request.member.has_perm("sales.override_manager")
         ):
@@ -245,7 +245,7 @@ class OrderAdmin(admin.ModelAdmin):
         return queryset
 
     def has_add_permission(self, request):
-        if not (
+        if request.member and not (
             request.member.is_superuser
             or request.member.has_perm("sales.override_manager")
         ):
@@ -307,9 +307,11 @@ class OrderAdmin(admin.ModelAdmin):
             )
         if db_field.name == "shift":
             field.queryset = Shift.objects.filter(locked=False)
-            if not (
-                request.member.is_superuser
-                or request.member.has_perm("sales.override_manager")
+            if request.member and not (
+                (
+                    request.member.is_superuser
+                    or request.member.has_perm("sales.override_manager")
+                )
             ):
                 field.queryset = field.queryset.filter(
                     managers__in=request.member.get_member_groups()
@@ -318,8 +320,11 @@ class OrderAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         if not (
-            request.member.is_superuser
-            or request.member.has_perm("sales.override_manager")
+            request.member
+            and (
+                request.member.is_superuser
+                or request.member.has_perm("sales.override_manager")
+            )
         ):
             self.message_user(
                 request,
@@ -331,7 +336,7 @@ class OrderAdmin(admin.ModelAdmin):
     def change_view(self, request, object_id, form_url="", extra_context=None):
         object_id
         return super().change_view(request, object_id, form_url, extra_context)
-        # TODO WARN WHEN ORDER IS AGE RESTRICTED AND PAYER IS UNDERAGE
+        # @todo WARN WHEN ORDER IS AGE RESTRICTED AND PAYER IS UNDERAGE
 
     def order_description(self, obj):
         if obj.order_description:
