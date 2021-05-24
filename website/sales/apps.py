5683b8decddc63308ab72a1af8e9b21198403231
Job Doesburg <job.doesburg@gmail.com>
2021-05-24 20:46:34 +0200

    Register sales payable

--- website/sales/apps.py
+++ website/sales/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class SalesConfig(AppConfig):
     name = "sales"
+
+    def ready(self):
+        # pylint: disable=unused-import,import-outside-toplevel
+        from .payables import register
+
+        register()
