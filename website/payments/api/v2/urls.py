ddfc6b9699b212c241c671428d8127033273b825
Job Doesburg <job.doesburg@gmail.com>
2021-06-20 14:13:38 +0200

    Add tpay status API endpoint (#1728)

--- website/payments/api/v2/urls.py
+++ website/payments/api/v2/urls.py
@@ -5,12 +5,18 @@ from payments.api.v2.views import (
     PaymentListView,
     PaymentDetailView,
     PayableDetailView,
+    PaymentUserCurrentView,
 )
 
 app_name = "payments"
 
 urlpatterns = [
     path("payments/", PaymentListView.as_view(), name="payments-list"),
+    path(
+        "payments/users/me/",
+        PaymentUserCurrentView.as_view(),
+        name="payments-tpay-status",
+    ),
     path("payments/<uuid:pk>/", PaymentDetailView.as_view(), name="payment-detail"),
     path(
         "payments/payables/<str:app_label>/<str:model_name>/<str:payable_pk>/",
