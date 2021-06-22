6405b36b72330f4064ab3133358d1370af127037
Job Doesburg <job.doesburg@gmail.com>
2021-06-22 13:36:51 +0200

    Remove redundant check

--- website/pizzas/views.py
+++ website/pizzas/views.py
@@ -66,7 +66,7 @@ def place_order(request):
         if not obj:
             obj = FoodOrder(food_event=event, member=request.member)
         obj.product = product
-        if obj.payment and obj.can_be_changed:
+        if obj.payment:
             delete_payment(obj.payment)
         obj.save()
     return redirect("pizzas:index")
