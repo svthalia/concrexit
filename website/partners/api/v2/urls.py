b9bbdb6862dae067015a64cc066c55c2172e9d81
Sébastiaan <se_bastiaan@outlook.com>
2021-08-02 10:57:37 +0200

    Add partners and partner events API v2 (#1843)
    * Add partners and partner events API v2
    
    * Fix black

--- website/partners/api/v2/urls.py
+++ website/partners/api/v2/urls.py
@@ -1,5 +1,22 @@
 """Partners app API v2 urls."""
+from django.urls import path
+
+from partners.api.v2.views import (
+    PartnerEventListView,
+    PartnerListView,
+    PartnerEventDetailView,
+)
 
 app_name = "partners"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "partners/events/", PartnerEventListView.as_view(), name="partner-events-list"
+    ),
+    path(
+        "partners/events/<int:pk>/",
+        PartnerEventDetailView.as_view(),
+        name="partner-events-detail",
+    ),
+    path("partners/", PartnerListView.as_view(), name="partners-list"),
+]
---
+++ website/partners/api/v2/views.py
@@ -0,0 +1,46 @@
+from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
+from rest_framework import filters as framework_filters
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from partners.api.v2 import filters
+from partners.api.v2.serializers.partner import PartnerSerializer
+from partners.api.v2.serializers.partner_event import PartnerEventSerializer
+from partners.models import PartnerEvent, Partner
+
+
+class PartnerEventListView(ListAPIView):
+    """Returns an overview of all partner events."""
+
+    serializer_class = PartnerEventSerializer
+    queryset = PartnerEvent.objects.filter(published=True)
+    filter_backends = (
+        framework_filters.OrderingFilter,
+        framework_filters.SearchFilter,
+        filters.PartnerEventDateFilter,
+    )
+    ordering_fields = ("start", "end", "title")
+    search_fields = ("title",)
+    permission_classes = [IsAuthenticatedOrTokenHasScope]
+    required_scopes = ["partners:read"]
+
+
+class PartnerEventDetailView(RetrieveAPIView):
+    """Returns a single partner event."""
+
+    serializer_class = PartnerEventSerializer
+    queryset = PartnerEvent.objects.filter(published=True)
+    permission_classes = [IsAuthenticatedOrTokenHasScope]
+    required_scopes = ["partners:read"]
+
+
+class PartnerListView(ListAPIView):
+    serializer_class = PartnerSerializer
+    queryset = Partner.objects.filter(is_active=True)
+    filter_backends = (
+        framework_filters.OrderingFilter,
+        framework_filters.SearchFilter,
+    )
+    ordering_fields = ("name", "pk")
+    search_fields = ("name",)
+    permission_classes = [IsAuthenticatedOrTokenHasScope]
+    required_scopes = ["partners:read"]
