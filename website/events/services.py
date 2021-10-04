c07511750e66d1dd241406f7c74975da6e2edbd8
Jelle Besseling <jelle@pingiun.com>
2021-10-04 16:57:02 +0200

    Set oldest date for event info to 5 years

--- website/events/services.py
+++ website/events/services.py
@@ -342,7 +342,7 @@ def generate_category_statistics():
 def execute_data_minimization(dry_run=False):
     """Delete information about very old events."""
     # Sometimes years are 366 days of course, but better delete 1 or 2 days early than late
-    deletion_period = timezone.now().date() - timezone.timedelta(days=(365 * 7))
+    deletion_period = timezone.now().date() - timezone.timedelta(days=(365 * 5))
 
     queryset = EventRegistration.objects.filter(event__end__lte=deletion_period).filter(
         Q(payment__isnull=False) | Q(member__isnull=False) | ~Q(name__exact="<removed>")
