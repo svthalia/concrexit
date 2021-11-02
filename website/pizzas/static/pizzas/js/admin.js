060b81fb31a75defe4c97060658d42cd8b9e86ac
Job Doesburg <job.doesburg@gmail.com>
2021-11-02 18:52:35 +0100

    Fix pizza order admin

--- website/pizzas/static/pizzas/js/admin.js
+++ website/pizzas/static/pizzas/js/admin.js
@@ -3,7 +3,7 @@ django.jQuery(function () {
     var payment_previous;
     $('select[name=payment]').on('focus', function () {
         payment_previous = this.value;
-    }).change(function() {
+    }).change(function () {
         var select = $(this);
         var url = $(this).parents('tr').data("payable-url");
         var none = $(this).data('none');
@@ -11,6 +11,7 @@ django.jQuery(function () {
         if (payment_previous === $(this).val()) {
             return;
         }
+        $('table').trigger('update', false).trigger('sortReset');
 
         var successCallback = function(data) {
             if (!data) {
@@ -19,10 +20,12 @@ django.jQuery(function () {
                 select.addClass('paid');
             }
             select.val(data ? data.payment.type: none);
+            $('table').trigger('update', false);
         };
 
         var failCallback = function(xhr) {
             select.val(payment_previous);
+            $('table').trigger('update', false);
 
             if (payment_previous === none) {
                 select.removeClass('paid');
@@ -41,10 +44,83 @@ django.jQuery(function () {
             request('DELETE', url, null, successCallback, failCallback);
         } else {
             $(this).addClass('paid');
-            request('PATCH', url, { payment_type: $(this).val() }, successCallback, failCallback);
+            request('PATCH', url, {payment_type: $(this).val()}, successCallback, failCallback);
         }
-
     });
+
+    $('a.deletelink').click(function () {
+        if (confirm(gettext('Are you sure you want to delete this order?'))) {
+            var id = $(this).parents('tr').data('id');
+            var button = $(this);
+            var url = $(this).parents('tr').data("url");
+
+            var successCallback = function (data) {
+                button.parents('tr').remove();
+                $('table').trigger('update');
+            }
+            var failCallback = function (data) {
+                var data = $.parseJSON(xhr.responseText);
+                if (data.message !== undefined) {
+                    alert(data.message);
+                } else if (data.payment !== undefined) {
+                    alert(data.payment.join('\n'));
+                }
+            }
+
+            request('DELETE', url, null, successCallback, failCallback);
+        }
+    });
+    $('#searchbar').on('input', function () {
+        var input = this.value.toLowerCase();
+        $('#result_list tbody tr').each(function (i, e) {
+            var tr = $(this);
+            var show = false;
+            $(this).find('td').each(function (j, t) {
+                if (j > 2) {
+                    return;
+                }
+                if ($(t).text().toLowerCase().indexOf(input) !== -1) {
+                    show = true;
+                }
+            });
+            if (show) {
+                tr.show();
+            } else {
+                tr.hide();
+            }
+        });
+    });
+    $.tablesorter.addParser({
+        id: 'payment',
+        is: function(s) {
+            return false;
+        },
+        format: function(s, t, node) {
+            const val = node.firstElementChild.value;
+            if (val === 'no_payment') {
+                return 'z';
+            }
+            return val;
+        },
+        type: "text",
+    });
+
+    $.tablesorter.addParser({
+        id: "sortval",
+        is: function(s) {
+            return false;
+        },
+        format: function(s, t, node) {
+            return $(node).data("sortval");
+        },
+        type: "text"
+    });
+
+    $("table").tablesorter({
+        sortList: [[1,0]],
+        cssHeader: 'sortable',
+    });
+
 });
 
 function request(method, url, data, success, error) {
