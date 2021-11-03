6efdb9953714baf463de83107337049763aef2a2
Wouter Doeland <git@wouterdoeland.nl>
2021-11-03 17:45:08 +0100

    Improve vacancy page layout (#1939)
    * Improve vacancy page layout
    
    * Fix vacancy image centering
    
    * Fix vacancy card
    
    * Fix partner models w black
    
    * Fix migrations

--- website/partners/models.py
+++ website/partners/models.py
@@ -138,6 +138,14 @@ class Vacancy(models.Model):
     link = models.CharField(
         _("link"), max_length=255, blank=True, validators=[URLValidator()]
     )
+    location = models.CharField(_("location"), max_length=255, null=True, blank=True)
+    keywords = models.TextField(
+        _("keywords"),
+        default="",
+        help_text="Comma separated list of keywords, for example: "
+        "Django,Python,Concrexit",
+        blank=True,
+    )
 
     partner = models.ForeignKey(
         Partner,
