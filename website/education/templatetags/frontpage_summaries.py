bfb993ea5a2faae9daa1744d1b5f157f6237246f
Job Doesburg <job.doesburg@gmail.com>
2021-12-04 14:27:26 +0100

    Add summaries to frontpage, re-show event cards

---
+++ website/education/templatetags/frontpage_summaries.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+from education.models import Summary
+from events import services
+from events.models import Event
+
+register = template.Library()
+
+
+@register.inclusion_tag("education/frontpage_summaries.html", takes_context=True)
+def render_frontpage_summaries(context, summaries=None):
+    if summaries is None:
+        summaries = Summary.objects.filter(accepted=True,).order_by("uploader_date")[:6]
+
+    return {"summaries": summaries}
