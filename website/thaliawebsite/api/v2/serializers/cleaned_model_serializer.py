ded8b8cdca732d64a68330e594a7b6f6e46ed368
Job Doesburg <job.doesburg@gmail.com>
2021-12-22 21:59:29 +0100

    Fix CleanedModelSerializer (#2116)

--- website/thaliawebsite/api/v2/serializers/cleaned_model_serializer.py
+++ website/thaliawebsite/api/v2/serializers/cleaned_model_serializer.py
@@ -4,8 +4,12 @@ from rest_framework import serializers
 class CleanedModelSerializer(serializers.ModelSerializer):
     """Custom ModelSerializer that explicitly clean's a model before it is saved."""
 
-    def validate(self, attrs):
-        super().validate(attrs)
-        instance = self.Meta.model(**attrs)
+    def create(self, validated_data, **kwargs):
+        self.Meta.model(**validated_data).clean()
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data, **kwargs):
+        for key, val in validated_data.items():
+            setattr(instance, key, val)
         instance.clean()
-        return attrs
+        return super().update(instance, validated_data)
