575d24eb1efe18a4e870941d3b368712e3183dff
Job Doesburg <job.doesburg@gmail.com>
2022-02-16 20:15:02 +0100

    Display random current summaries instead of always the latest

--- website/education/templatetags/frontpage_summaries.py
+++ website/education/templatetags/frontpage_summaries.py
@@ -1,10 +1,6 @@
 from django import template
-from django.utils import timezone
-from django.utils.translation import gettext as _
 
 from education.models import Summary
-from events import services
-from events.models import Event
 
 register = template.Library()
 
@@ -12,8 +8,8 @@ register = template.Library()
 @register.inclusion_tag("education/frontpage_summaries.html", takes_context=True)
 def render_frontpage_summaries(context, summaries=None):
     if summaries is None:
-        summaries = Summary.objects.filter(accepted=True,).order_by(
-            "-uploader_date"
-        )[:6]
+        summaries = Summary.objects.filter(
+            accepted=True, course__until__isnull=True
+        ).order_by("?")[:6]
 
     return {"summaries": summaries}
