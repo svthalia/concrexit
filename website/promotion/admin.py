451339514b857b33ef31b8646669150155e9bc42
nvoers <nick-vanoers@hotmail.com>
2022-03-03 12:39:22 +0100

    added update status actions

--- website/promotion/admin.py
+++ website/promotion/admin.py
@@ -17,11 +17,40 @@ class PromotionRequestAdmin(admin.ModelAdmin):
     )
     date_hierarchy = "publish_date"
     form = PromotionRequestForm
+    actions = ["mark_not_started", "mark_started", "mark_finished", "mark_published"]
 
     def has_change_permission(self, request, obj=None):
         if obj is not None and not is_organiser(request.member, obj.event):
             return False
         return super().has_change_permission(request, obj)
+    
+    def mark_not_started(self, request, queryset):
+        """Change the status of the event to published."""
+        self._change_published(request, queryset, PromotionRequest.NOT_STARTED)
+
+    mark_not_started.short_description = ("Mark requests as not started")
+
+    def mark_started(self, request, queryset):
+        """Change the status of the event to published."""
+        self._change_published(request, queryset, PromotionRequest.STARTED)
+
+    mark_started.short_description = ("Mark requests as started")
+
+    def mark_finished(self, request, queryset):
+        """Change the status of the event to published."""
+        self._change_published(request, queryset, PromotionRequest.FINISHED)
+
+    mark_finished.short_description = ("Mark requests as finished")
+
+    def mark_published(self, request, queryset):
+        """Change the status of the event to published."""
+        self._change_published(request, queryset, PromotionRequest.PUBLISHED)
+
+    mark_published.short_description = ("Mark requests as published")
+
+    @staticmethod
+    def _change_published(request, queryset, status):
+        queryset.update(status=status)
 
 
 @admin.register(PromotionChannel)
