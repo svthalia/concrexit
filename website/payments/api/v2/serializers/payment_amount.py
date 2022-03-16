ef2d48986c65dfe4396ea8853e737c6f55d29317
Quinten Kock <quintenkock@gmail.com>
2022-03-16 19:51:02 +0100

    Implement PaymentAmountSerializer

---
+++ website/payments/api/v2/serializers/payment_amount.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from payments.models import PaymentAmountField
+
+class PaymentAmountSerializer(serializers.DecimalField):
+    def __init__(self, **kwargs):
+        kwargs["max_digits"] = 8
+        kwargs["decimal_places"] = 2
+        super().__init__(**kwargs)
