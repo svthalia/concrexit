9bba464f37e8e65a035eeb705df920690d90c26d
Job Doesburg <job.doesburg@gmail.com>
2022-03-17 10:15:18 +0100

    More tests

--- website/payments/tests/test_payables.py
+++ website/payments/tests/test_payables.py
@@ -2,7 +2,8 @@ from unittest.mock import MagicMock
 
 from django.test import TestCase
 
-from payments import payables, NotRegistered, prevent_saving, PaymentError
+from payments import payables, NotRegistered, prevent_saving, PaymentError, \
+    Payable
 from payments.models import Payment
 from payments.tests.__mocks__ import MockModel, MockPayable
 
@@ -26,41 +27,53 @@ class ImmutablePayablesTest(TestCase):
 
     def test_prevent_unlinking_payment_from_payable(self):
         model_before = MockModel(payer=None)
+        model_after = MockModel(payer=None)
         model_before.payment = Payment.objects.create(
             type=Payment.CARD, amount=2, notes="test payment", topic="test topic"
         )
-
-        model_after = MockModel(payer=None)
-        model_after.payment = None
-
         MockPayable.get_payment = MagicMock(return_value=model_before.payment)
         MockModel.objects.get = MagicMock(return_value=model_before)
 
-        with self.assertRaises(PaymentError) as test:
-            prevent_saving(MockModel, model_after)
+        with self.subTest("Unlinking a payment"):
+            model_after.payment = None
 
-        exception = test.exception
-        self.assertEqual(
-            str(exception), "You are trying to unlink a payment from its payable."
-        )
+            with self.assertRaises(PaymentError) as test:
+                prevent_saving(MockModel, model_after)
 
-    def test_prevent_saving_payable_with_immutable_field(self):
+            exception = test.exception
+            self.assertEqual(
+                str(exception), "You are trying to unlink a payment from its payable."
+            )
+
+        with self.subTest("Changing an immutable model field after payment"):
+            model_after.payment = model_before.payment
+            model_after.test_field = "changed"
+
+            with self.assertRaises(PaymentError) as test:
+                prevent_saving(MockModel, model_after)
+
+            exception = test.exception
+            self.assertEqual(str(exception), "Cannot change this model")
+
+
+    def test_prevent_saving_no_existing_model(self):
+        model = MockModel(payer=None)
+        model.pk = None
+        prevent_saving(MockModel, model) # this should not crash
+
+
+    def test_allow_adding_a_payment_to_unpaid_model(self):
         model_before = MockModel(payer=None)
-        model_before.payment = Payment.objects.create(
+        model_before.payment = None
+
+        model_after = MockModel(payer=None)
+        model_after.payment = Payment.objects.create(
             type=Payment.CARD, amount=2, notes="test payment", topic="test topic"
         )
 
-        model_after = MockModel(payer=None)
-        model_after.payment = model_before.payment
-        model_after.test_field = "changed"
-
         MockModel.objects.get = MagicMock(return_value=model_before)
+        prevent_saving(MockModel, model_after)
 
-        with self.assertRaises(PaymentError) as test:
-            prevent_saving(MockModel, model_after)
-
-        exception = test.exception
-        self.assertEqual(str(exception), "Cannot change this model")
 
     def test_mutable_model(self):
         payable = payables.get_payable(MockModel)
@@ -69,6 +82,8 @@ class ImmutablePayablesTest(TestCase):
         self.assertFalse(payable.immutable_after_payment)
         MockPayable.immutable_after_payment = True
         self.assertTrue(payable.immutable_after_payment)
+
+        self.assertFalse(Payable.immutable_after_payment)
 
     def test_immutable_fields(self):
         payable = payables.get_payable(MockModel)
@@ -79,6 +94,10 @@ class ImmutablePayablesTest(TestCase):
         )
         MockPayable.immutable_model_fields_after_payment = []
         self.assertEqual([], payable.immutable_model_fields_after_payment)
+
+        self.assertEqual([], Payable.immutable_model_fields_after_payment)
+
+
 
 
 # def test_allow_saving_payable_without_immutable_field(self):
