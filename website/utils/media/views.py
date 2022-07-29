8e5ef9878c3424a898a04918f6f190fa7f2e81d6
Sébastiaan <se_bastiaan@outlook.com>
2022-07-29 17:49:11 +0200

    Only check files a single time

--- website/utils/media/views.py
+++ website/utils/media/views.py
@@ -2,13 +2,13 @@
 from datetime import timedelta
 
 from PIL import Image, ImageOps
-from django.conf import settings
 from django.core import signing
 from django.core.exceptions import PermissionDenied
 from django.core.files.storage import get_storage_class
 from django.core.signing import BadSignature
 from django.http import Http404
 from django.shortcuts import redirect
+from django.utils import timezone
 from django_sendfile import sendfile
 
 from utils.media.services import save_image
@@ -71,24 +71,30 @@ def get_thumbnail(request, request_path):
     """
     # Get image information from signature
     # raises PermissionDenied if bad signature
-    query = ""
     sig_info = _get_signature_info(request)
     storage = get_storage_class(sig_info["storage"])()
-    is_public = sig_info["storage"] == settings.PUBLIC_FILE_STORAGE
 
     if not sig_info["thumb_path"].endswith(request_path):
         # 404 if the file does not exist
         raise Http404("Media not found.")
 
-    if not storage.exists(sig_info["name"]):
+    original_modified_time = timezone.make_aware(timezone.datetime.min)
+    thumb_modified_time = original_modified_time
+    # noinspection PyBroadException
+    try:
+        original_modified_time = storage.get_modified_time(sig_info["name"])
+        thumb_modified_time = storage.get_modified_time(sig_info["thumb_path"])
+    except:
+        # One of the files probably does not exist
+        pass
+
+    if original_modified_time.timestamp() == 0:
         raise Http404
 
     # Check if directory for thumbnail exists, if not create it
     # os.makedirs(os.path.dirname(full_thumb_path), exist_ok=True)
     # Skip generating the thumbnail if it exists
-    if not storage.exists(sig_info["thumb_path"]) or storage.get_modified_time(
-        sig_info["name"]
-    ) > storage.get_modified_time(sig_info["thumb_path"]):
+    if original_modified_time > thumb_modified_time:
         storage.delete(sig_info["thumb_path"])
 
         # Create a thumbnail from the original_path, saved to thumb_path
