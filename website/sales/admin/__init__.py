95e895b35998359589e8a483d4dc0fdc697bd2a5
Job Doesburg <job.doesburg@gmail.com>
2022-11-12 00:56:18 +0100

    First demo of a separated member proxy model admin

--- website/sales/admin/__init__.py
+++ website/sales/admin/__init__.py
@@ -1,3 +1,4 @@
 from sales.admin.order_admin import *
 from sales.admin.product_admin import *
+from sales.admin.sales_user import *
 from sales.admin.shift_admin import *
---
+++ website/sales/admin/sales_user.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.contrib.admin import register
+
+from sales import services
+from sales.admin import OrderInline
+from sales.models.order import SalesUser
+
+
+class SalesUserOrderInline(OrderInline):
+    fk_name = "payer"
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+@register(SalesUser)
+class SalesUserAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    list_display = (
+        "__str__",
+        "is_adult",
+    )
+
+    fields = (
+        "__str__",
+        "is_adult",
+    )
+
+    readonly_fields = ("is_adult",)
+
+    def is_adult(self, obj):
+        return services.is_adult(obj)
+
+    is_adult.boolean = True
+
+    inlines = [SalesUserOrderInline]
