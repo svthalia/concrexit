c274fcee33171c3d4e0773ab4eb4d84f41598955
Jelle Besseling <jelle@pingiun.com>
2022-11-29 14:08:11 +0100

    Try running migrations as appuser

--- infra/docker/docker-entrypoint.sh
+++ infra/docker/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 chown -R appuser /media
-MANAGE_PY=1 /venv/bin/python manage.py migrate --no-input
+MANAGE_PY=1 runuser -u appuser -- /venv/bin/python manage.py migrate --no-input
 exec runuser -u appuser -- /venv/bin/gunicorn --bind 0.0.0.0:8000 --log-level info -w 4 --capture-output thaliawebsite.wsgi:application
