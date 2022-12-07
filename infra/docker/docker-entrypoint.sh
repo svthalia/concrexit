596f866ea4e13fb6432d02e808a0de57c963a71a
Jelle Besseling <jelle@pingiun.com>
2022-12-07 19:50:26 +0100

    Increase gunicorn timeout

--- infra/docker/docker-entrypoint.sh
+++ infra/docker/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 chown -R appuser /media
 MANAGE_PY=1 runuser -u appuser -- /venv/bin/python manage.py migrate --no-input
-exec runuser -u appuser -- /venv/bin/gunicorn --bind 0.0.0.0:8000 --log-level info --workers 4 --threads 100 --capture-output thaliawebsite.wsgi:application
+exec runuser -u appuser -- /venv/bin/gunicorn --bind 0.0.0.0:8000 --log-level info --workers 4 --threads 20 --timeout 240 --capture-output thaliawebsite.wsgi:application
