8857816b32ee6fd3cf2ea4ab557d95b246a4eb87
Wouter Doeland <git@wouterdoeland.nl>
2023-01-25 21:19:00 +0100

    Fix local partners in newsletter (#2460)
    * Fix partners in newsletter
    
    * make fmt
    
    * Improve code quality
    
    Co-authored-by: Sébastiaan <se_bastiaan@outlook.com>

--- website/newsletters/services.py
+++ website/newsletters/services.py
@@ -1,5 +1,6 @@
 import base64
 import logging
+import math
 import os
 from io import BytesIO
 
@@ -34,7 +35,7 @@ def write_to_file(pk, lang, html_message):
 def save_to_disk(newsletter):
     """Write the newsletter as HTML to file (in all languages)."""
     main_partner = Partner.objects.filter(is_main_partner=True).first()
-    local_partners = Partner.objects.filter(is_local_partner=True)
+    local_partners = split_local_partners()
 
     html_template = get_template("newsletters/email.html")
 
@@ -107,3 +108,18 @@ def send_newsletter(newsletter):
     message.send()
 
     save_to_disk(newsletter)
+
+
+def split_local_partners():
+    all_local_partners = Partner.objects.filter(is_local_partner=True).order_by("?")
+    local_partner_count = len(all_local_partners)
+    local_partners = []
+    for i in range(math.floor(local_partner_count / 2)):
+        local_partners.append(
+            [all_local_partners[i * 2], all_local_partners[i * 2 + 1]]
+        )
+
+    if local_partner_count % 2 != 0:
+        local_partners.append([all_local_partners[local_partner_count - 1]])
+
+    return local_partners
