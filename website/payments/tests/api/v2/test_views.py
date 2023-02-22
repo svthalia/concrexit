e0cde5a20969418f38fc74e43928986d31041cee
Joost van Esch <joostvanesch@pm.me>
2023-02-22 20:44:27 +0100

    Added settled filter to payments api

---
+++ website/payments/tests/api/v2/test_views.py
@@ -0,0 +1,101 @@
+from unittest import mock
+from unittest.mock import ANY, MagicMock, Mock, PropertyMock, patch
+
+from django.apps import apps
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase, override_settings
+from django.urls import reverse
+from django.utils import timezone
+
+from freezegun import freeze_time
+
+from members.models import Member
+from payments import payables
+from payments.exceptions import PaymentError
+from payments.models import BankAccount, Batch, Payment, PaymentUser
+from payments.tests.__mocks__ import MockModel
+from payments.tests.test_services import MockPayable
+
+
+@freeze_time("2019-04-01")
+@override_settings(SUSPEND_SIGNALS=True)
+class PaymentListViewTest(TestCase):
+    """Test for the PaymentListView."""
+
+    fixtures = ["members.json"]
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.login_user = PaymentUser.objects.filter(last_name="Wiggers").first()
+        cls.account1 = BankAccount.objects.create(
+            owner=cls.login_user,
+            initials="J1",
+            last_name="Test",
+            iban="NL91ABNA0417164300",
+            valid_from="2019-03-01",
+            signature="sig",
+            mandate_no="11-2",
+        )
+        cls.payment1 = Payment.objects.create(
+            created_at=timezone.datetime(year=2019, month=3, day=1),
+            paid_by=cls.login_user,
+            processed_by=cls.login_user,
+            notes="Testing Payment 1",
+            amount=10,
+            type=Payment.TPAY,
+        )
+
+    def setUp(self):
+        self.account1.refresh_from_db()
+        self.payment1.refresh_from_db()
+        self.client = Client()
+        self.client.force_login(self.login_user)
+
+    def test_settled_filter(self):
+        """Test if the view shows payments."""
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list"),
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Testing Payment 1")
+
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list") + "?settled=yes",
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertNotContains(response, "Testing Payment 1")
+
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list") + "?settled=no",
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Testing Payment 1")
+
+        self.payment1.batch = Batch.objects.create(processed=False)
+        self.payment1.save()
+        self.payment1.batch.processed = True
+        self.payment1.batch.save()
+
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list"),
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Testing Payment 1")
+
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list") + "?settled=yes",
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "Testing Payment 1")
+
+        response = self.client.get(
+            reverse("api:v2:payments:payments-list") + "?settled=no",
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertNotContains(response, "Testing Payment 1")
