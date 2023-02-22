b15b7e44b5f94f1e63f1ae19497f5c60c88204c4
nvoers <nick-vanoers@hotmail.com>
2023-02-22 20:46:40 +0100

    removed django-moneybird

--- website/moneybirdsynchronization/admin.py
+++ website/moneybirdsynchronization/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 from .models import Contact
-from moneybird.admin import MoneybirdResourceModelAdminMixin
 
 @admin.register(Contact)
-class ContactAdmin(MoneybirdResourceModelAdminMixin, admin.ModelAdmin):
+class ContactAdmin(admin.ModelAdmin):
     """Manage moneybird contacts."""
 
     list_display = ("first_name", "last_name")
