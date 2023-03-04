5e798a478bdf64a7606e68305bb42fd71b65edfb
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-03-04 13:51:29 +0100

    Fix request.member evaluated too early in API calls (#2846)

--- website/announcements/middleware.py
+++ website/announcements/middleware.py
@@ -2,6 +2,13 @@ from django.apps import apps
 
 
 class AnnouncementMiddleware:
+    """Adds the _announcements attribute to requests.
+
+    This middleware lets apps add announcements to a request, by providing an
+    AppConfig.announcements method that takes the request as argument, and returns
+    a list of announcements to add.
+    """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
