eb2ba0e6b69614df71faafac76b343b501bcdb98
Noah <noah@langenheim.de>
2023-03-15 20:28:55 +0100

    Moved EventDocuments from documents app to events app

--- website/documents/admin.py
+++ website/documents/admin.py
@@ -7,11 +7,11 @@ from documents import forms
 from documents.models import (
     AnnualDocument,
     AssociationDocument,
-    EventDocument,
     GeneralMeeting,
     Minutes,
     MiscellaneousDocument,
 )
+
 from documents.services import is_owner
 
 
@@ -91,34 +91,6 @@ class AssociationDocumentAdmin(ModelAdmin):
         "__str__",
         "members_only",
     )
-
-
-@admin.register(EventDocument)
-class EventDocumentAdmin(ModelAdmin):
-    """Manage the event documents."""
-
-    form = forms.EventDocumentForm
-    list_filter = (
-        "created",
-        "last_updated",
-        "members_only",
-    )
-    list_display = (
-        "__str__",
-        "members_only",
-    )
-
-    def has_change_permission(self, request, obj=None):
-        """Only allow access to the change form if the user is an owner."""
-        if obj is not None and not is_owner(request.member, obj):
-            return False
-        return super().has_change_permission(request, obj)
-
-    def has_delete_permission(self, request, obj=None):
-        """Only allow delete access if the user is an owner."""
-        if obj is not None and not is_owner(request.member, obj):
-            return False
-        return super().has_delete_permission(request, obj)
 
 
 @admin.register(MiscellaneousDocument)
