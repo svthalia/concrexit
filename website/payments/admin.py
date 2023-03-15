svthalia/concrexit

commit 8cdcecda2957f1a92c0199374540a066c88b26fc

Make apps depend on settings.AUTH_USER_MODEL instead of Member model (#2768)
* Make apps depend on settings.AUTH_USER_MODEL instead of Member model

* Fix sales and tests

* Fix formatting

* Fix

Co-authored-by: Job Doesburg <job.doesburg@gmail.com>

website/payments/admin.py
@@ -89,12 +89,14 @@ class PaymentAdmin(admin.ModelAdmin):
     ]
 
     @staticmethod
-    def _member_link(member: PaymentUser) -> str:
+    def _member_link(user: PaymentUser) -> str:
+        url = reverse(
+            f"admin:{user._meta.app_label}_{user._meta.model_name}_change",
+            args=[user.id],
+        )
         return (
-            format_html(
-                "<a href='{}'>{}</a>", member.get_absolute_url(), member.get_full_name()
-            )
-            if member
+            format_html("<a href='{}'>{}</a>", url, user.get_full_name())
+            if user
             else None
         )
 
@@ -563,6 +565,9 @@ class BankAccountInline(admin.TabularInline):
     def has_change_permission(self, request, obj=None):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 class PaymentInline(admin.TabularInline):
     model = Payment
@@ -585,6 +590,9 @@ class PaymentInline(admin.TabularInline):
         return False
 
     def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
         return False
 
 

website/education/migrations/0015_alter_exam_uploader_alter_summary_uploader.py
@@ -1,0 +1,36 @@
+# Generated by Django 4.1.4 on 2023-01-18 19:28
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("education", "0014_remove_multilang_field"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="exam",
+            name="uploader",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="uploader",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="summary",
+            name="uploader",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="uploader",
+            ),
+        ),
+    ]

website/education/models.py
@@ -1,10 +1,10 @@
 """The models defined by the education package."""
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from members.models import Member
 from utils.snippets import datetime_to_lectureyear
 
 
@@ -82,7 +82,7 @@ class Exam(models.Model):
     name = models.CharField(max_length=255, verbose_name=_("exam name"), blank=True)
 
     uploader = models.ForeignKey(
-        Member,
+        settings.AUTH_USER_MODEL,
         verbose_name=_("uploader"),
         on_delete=models.SET_NULL,
         null=True,
@@ -151,7 +151,7 @@ class Summary(models.Model):
     )
 
     uploader = models.ForeignKey(
-        Member,
+        settings.AUTH_USER_MODEL,
         verbose_name=_("uploader"),
         on_delete=models.SET_NULL,
         null=True,

website/payments/migrations/0020_alter_payment_paid_by_alter_payment_processed_by.py
@@ -1,0 +1,38 @@
+# Generated by Django 4.1.4 on 2023-01-18 19:28
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("payments", "0019_alter_payment_paid_by"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="payment",
+            name="paid_by",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="paid_payment_set",
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="paid by",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="payment",
+            name="processed_by",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="processed_payment_set",
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="processed by",
+            ),
+        ),
+    ]

website/payments/models.py
@@ -4,6 +4,7 @@ import uuid
 from decimal import Decimal
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -18,8 +19,6 @@ from localflavor.generic.countries.sepa import IBAN_SEPA_COUNTRIES
 from localflavor.generic.models import BICField, IBANField
 from queryable_properties.managers import QueryablePropertiesManager
 from queryable_properties.properties import AggregateProperty, queryable_property
-
-from members.models import Member
 
 
 def validate_not_zero(value):
@@ -44,7 +43,10 @@ class PaymentAmountField(models.DecimalField):
         super().__init__(**kwargs)
 
 
-class PaymentUser(Member):
+UserModel = get_user_model()
+
+
+class PaymentUser(UserModel):
     class Meta:
         proxy = True
         verbose_name = "payment user"
@@ -152,7 +154,7 @@ class Payment(models.Model):
     )
 
     paid_by = models.ForeignKey(
-        "members.Member",
+        settings.AUTH_USER_MODEL,
         models.CASCADE,
         verbose_name=_("paid by"),
         related_name="paid_payment_set",
@@ -161,7 +163,7 @@ class Payment(models.Model):
     )
 
     processed_by = models.ForeignKey(
-        "members.Member",
+        settings.AUTH_USER_MODEL,
         models.CASCADE,
         verbose_name=_("processed by"),
         related_name="processed_payment_set",

website/payments/tests/test_admin.py
@@ -95,7 +95,7 @@ class PaymentAdminTest(TestCase):
 
         self.assertEqual(
             self.admin.paid_by_link(payment),
-            f"<a href='/members/profile/{self.user.pk}'>Sébastiaan Versteeg</a>",
+            f"<a href='/admin/payments/paymentuser/{self.user.pk}/change/'>Sébastiaan Versteeg</a>",
         )
 
     def test_processed_by_link(self) -> None:
@@ -106,7 +106,7 @@ class PaymentAdminTest(TestCase):
 
         self.assertEqual(
             self.admin.processed_by_link(payment1),
-            f"<a href='/members/profile/{self.user.pk}'>Sébastiaan Versteeg</a>",
+            f"<a href='/admin/payments/paymentuser/{self.user.pk}/change/'>Sébastiaan Versteeg</a>",
         )
 
     def test_delete_model_succeed(self) -> None:

website/payments/tests/test_models.py
@@ -547,5 +547,5 @@ class BlacklistedPaymentUserTest(TestCase):
         member.disallow_tpay()
         self.assertEqual(
             str(member.blacklistedpaymentuser),
-            "Thom Wiggers (thom) (blacklisted from using Thalia Pay)",
+            "thom (blacklisted from using Thalia Pay)",
         )

website/photos/migrations/0018_alter_like_member.py
@@ -1,0 +1,25 @@
+# Generated by Django 4.1.4 on 2023-01-18 19:28
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("photos", "0017_alter_like_unique_together"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="like",
+            name="member",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+    ]

commit 77a09f8286686cd0d2b8cfdff4ea9703a5448cce

Fix a small bug introduced in #2768

website/payments/admin.py
@@ -90,12 +90,15 @@ class PaymentAdmin(admin.ModelAdmin):
 
     @staticmethod
     def _member_link(user: PaymentUser) -> str:
-        url = reverse(
-            f"admin:{user._meta.app_label}_{user._meta.model_name}_change",
-            args=[user.id],
-        )
         return (
-            format_html("<a href='{}'>{}</a>", url, user.get_full_name())
+            format_html(
+                "<a href='{}'>{}</a>",
+                reverse(
+                    f"admin:{user._meta.app_label}_{user._meta.model_name}_change",
+                    args=[user.id],
+                ),
+                user.get_full_name(),
+            )
             if user
             else None
         )