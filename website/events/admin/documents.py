e005cf285f99c4a1063e7f1f654ebff81c20963d
Noah <noah@langenheim.de>
2023-03-16 12:41:10 +0100

    Added changes to files that were forgotton in previous commit, such that now moving Event documents from doucments app to events app should actually work

---
+++ website/events/admin/documents.py
@@ -0,0 +1,31 @@
+from events.models.documents import EventDocument
+from events.forms import EventDocumentForm
+from documents.services import is_owner
+
+
+@admin.register(EventDocument)
+class EventDocumentAdmin(ModelAdmin):
+    """Manage the event documents."""
+
+    form = EventDocumentForm
+    list_filter = (
+        "created",
+        "last_updated",
+        "members_only",
+    )
+    list_display = (
+        "__str__",
+        "members_only",
+    )
+
+    def has_change_permission(self, request, obj=None):
+        """Only allow access to the change form if the user is an owner."""
+        if obj is not None and not is_owner(request.member, obj):
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        """Only allow delete access if the user is an owner."""
+        if obj is not None and not is_owner(request.member, obj):
+            return False
+        return super().has_delete_permission(request, obj)
