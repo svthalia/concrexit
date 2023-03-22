bc14ea23e82b8e82d8e18ff732a3a32537413f46
Job Doesburg <job.doesburg@gmail.com>
2023-03-22 20:15:45 +0100

    Catch error (#2906)
    * Catch extra google errors
    
    * Catch extra google errors
    
    * Catch extra google errors

--- website/activemembers/management/commands/init_gsuite_users.py
+++ website/activemembers/management/commands/init_gsuite_users.py
@@ -3,6 +3,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
+from google.auth.exceptions import RefreshError
 from googleapiclient.errors import HttpError
 
 from activemembers import emails
@@ -22,5 +23,5 @@ class Command(BaseCommand):
             try:
                 email, password = sync_service.create_user(member)
                 emails.send_gsuite_welcome_message(member, email, password)
-            except HttpError as e:
+            except (HttpError, RefreshError) as e:
                 logger.error("User %s could not be created: %s", member.username, e)
