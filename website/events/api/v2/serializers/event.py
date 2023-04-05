c272a429a2b4d7f7c7d970826986f6891d1d2d44
Quinten Kock <7569492+ColonelPhantom@users.noreply.github.com>
2023-04-05 19:09:55 +0200

    API event slug support (#2930)
    * Add event slug support to API
    
    * Add absolute URL to event API

--- website/events/api/v2/serializers/event.py
+++ website/events/api/v2/serializers/event.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from activemembers.api.v2.serializers.member_group import MemberGroupSerializer
 from documents.api.v2.serializers.document import DocumentSerializer
@@ -20,6 +21,8 @@ class EventSerializer(CleanedModelSerializer):
         model = Event
         fields = (
             "pk",
+            "slug",
+            "url",
             "title",
             "description",
             "caption",
@@ -57,6 +60,7 @@ class EventSerializer(CleanedModelSerializer):
     fine = PaymentAmountSerializer()
     documents = DocumentSerializer(many=True)
     user_permissions = serializers.SerializerMethodField("_user_permissions")
+    url = serializers.SerializerMethodField("_url")
 
     def _user_registration(self, instance: Event):
         if self.context["request"].member and len(instance.member_registration) > 0:
@@ -100,6 +104,19 @@ class EventSerializer(CleanedModelSerializer):
     def _user_permissions(self, instance):
         member = self.context["request"].member
         return services.event_permissions(member, instance, registration_prefetch=True)
+
+    def _url(self, instance: Event):
+        if instance.slug is None:
+            return reverse(
+                "events:event",
+                kwargs={"pk": instance.pk},
+                request=self.context["request"],
+            )
+        return reverse(
+            "events:event",
+            kwargs={"slug": instance.slug},
+            request=self.context["request"],
+        )
 
     def _maps_url(self, instance):
         return create_google_maps_url(instance.map_location, zoom=13, size="450x250")
