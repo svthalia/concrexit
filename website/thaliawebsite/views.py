59b720eed9a64894878c4b46f4191454dfc99c79
Quinten Kock <7569492+ColonelPhantom@users.noreply.github.com>
2023-04-06 20:12:26 +0200

    Add pagination for liked photos (#2877)
    * Add pagination for liked photos
    
    * Remove leading "—" in liked photos
    
    There was supposed to be an album title in front, however
    this is not being used, causing an ugly leading "—"
    
    * Create PagedView and paginated_page.html
    
    * Use PagedView class in photos app
    
    * Adjust tests to not overshoot pages in album index
    
    * Fix erroneous reference: photos -> page_obj
    
    * Remove pages block from simple_page
    
    * Fix indentation in paginated_page.html

--- website/thaliawebsite/views.py
+++ website/thaliawebsite/views.py
@@ -3,7 +3,7 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse, HttpResponseForbidden
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from django.views.generic.base import View
 
 
@@ -20,3 +20,34 @@ class TestCrashView(View):
             return HttpResponseForbidden("This is not for you")
         # Test exception so we don't care about it being too broad
         raise Exception("Test exception")  # pylint: disable=broad-exception-raised
+
+
+class PagedView(ListView):
+    """A ListView with automatic pagination."""
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        page = context["page_obj"].number
+        paginator = context["paginator"]
+
+        # Show the two pages before and after the current page
+        page_range_start = max(1, page - 2)
+        page_range_stop = min(page + 3, paginator.num_pages + 1)
+
+        # Add extra pages if we show less than 5 pages
+        page_range_start = min(page_range_start, page_range_stop - 5)
+        page_range_start = max(1, page_range_start)
+
+        # Add extra pages if we still show less than 5 pages
+        page_range_stop = max(page_range_stop, page_range_start + 5)
+        page_range_stop = min(page_range_stop, paginator.num_pages + 1)
+
+        page_range = range(page_range_start, page_range_stop)
+
+        context.update(
+            {
+                "page_range": page_range,
+            }
+        )
+
+        return context
