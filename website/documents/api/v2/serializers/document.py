cd86fc83ed63ffff88be809e159f640ffeb5e7ab
Marijn Meuleman <marijn2312@gmail.com>
2023-05-03 20:55:47 +0200

    Gets request info from context now (because not part of self)

--- website/documents/api/v2/serializers/document.py
+++ website/documents/api/v2/serializers/document.py
@@ -16,9 +16,11 @@ class DocumentSerializer(CleanedModelSerializer):
 
     def _url(self, instance):
         if instance.members_only and (
-            not self.request.user.is_authenticated
-            or not self.request.member.has_active_membership()
+            not self.context["request"].user.is_authenticated
+            or not self.context["request"].member.has_active_membership()
         ):
-            return self.request.build_absolute_uri(instance.get_absolute_url())
+            return self.context["request"].build_absolute_uri(
+                instance.get_absolute_url()
+            )
 
         return get_media_url(instance.file, absolute_url=True)
