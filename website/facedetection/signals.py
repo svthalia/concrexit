479fc3902a10f595d1ad651bc4c32ec8af7c8468
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-05-07 18:33:07 +0200

    Add trigger on uploads to album

--- website/facedetection/signals.py
+++ website/facedetection/signals.py
@@ -1,15 +1,30 @@
-from django.conf import settings
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from .models import ReferenceFace
+from photos.admin import uploaded_photos
+from utils.models.signals import suspendingreceiver
+
+from .models import FaceDetectionPhoto, ReferenceFace
 from .services import trigger_facedetection_lambda
 
 
-@receiver(
+@suspendingreceiver(
     post_save, sender=ReferenceFace, dispatch_uid="trigger_reference_face_analysis"
 )
 def trigger_reference_face_analysis(sender, instance, created, **kwargs):
     """Start the facedetection Lambda to extract a face encoding from the reference."""
     if created and instance.status == ReferenceFace.Status.PROCESSING:
         trigger_facedetection_lambda([instance])
+
+
+@suspendingreceiver(uploaded_photos, dispatch_uid="trigger_album_analysis")
+def trigger_album_analysis(sender, album, **kwargs):
+    """Start the facedetection Lambda on any new photos in the album."""
+    photos = FaceDetectionPhoto.objects.bulk_create(
+        FaceDetectionPhoto(photo=photo)
+        for photo in album.photo_set.filter(
+            facedetectionphoto__isnull=True,
+        )
+    )
+
+    if photos:
+        trigger_facedetection_lambda(photos)
