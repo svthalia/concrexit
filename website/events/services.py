08929d4f27b632ec4c46e0c10e8a5d43f7cfd096
Noah <noah@langenheim.de>
2023-05-17 21:37:58 +0200

    Fixed the migrations, such that the old exisiting data is copied into the new table

--- website/events/services.py
+++ website/events/services.py
@@ -498,3 +498,14 @@ def execute_data_minimisation(dry_run=False):
     if not dry_run:
         queryset.update(payment=None, member=None, name="<removed>")
     return queryset.all()
+
+
+def is_eventdocument_owner(member, event_doc):
+    if member and member.is_authenticated:
+        if member.is_superuser or member.has_perm("documents.override_owner"):
+            return True
+
+        if event_doc and member.has_perm("documents.change_document"):
+            return member.get_member_groups().filter(pk=event_doc.owner.pk).exists()
+
+    return False
