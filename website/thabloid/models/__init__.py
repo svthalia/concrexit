c8cb7b66a285ef2f3649f4de7f83c41900d3a4f6
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-06-07 20:36:23 +0200

    Fix linting

--- website/thabloid/models/__init__.py
+++ website/thabloid/models/__init__.py
@@ -1,2 +1,8 @@
 from .thabloid import Thabloid
 from .thabloid_user import BlacklistedThabloidUser, ThabloidUser
+
+__all__ = [
+    "Thabloid",
+    "ThabloidUser",
+    "BlacklistedThabloidUser",
+]
