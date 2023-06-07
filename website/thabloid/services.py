4939b6b4da537cfc3a3960e971a02beb09253a0d
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-06-07 20:30:13 +0200

    Document update_thabloid_blacklist_for_user

--- website/thabloid/services.py
+++ website/thabloid/services.py
@@ -2,6 +2,7 @@ from thabloid.models import ThabloidUser
 
 
 def update_thabloid_blacklist_for_user(form, instance, commit, *args, **kwargs):
+    """Update the Thabloid blacklist in the member profile edit form."""
     if form.cleaned_data["receive_thabloid"]:
         ThabloidUser.objects.get(pk=instance.user.pk).allow_thabloid()
     else:
