80df8fce7167c4b0ade90c4cbe7131d2b9e480a5
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-10-17 14:21:24 +0200

    Drop EntryPayable and test payables
    EntryPayable was only used when completing TPay registrations.
    Those now also use RegistrationPayable.

--- website/registrations/payables.py
+++ website/registrations/payables.py
@@ -2,10 +2,12 @@ from django.template.defaultfilters import date
 from django.utils.functional import classproperty
 
 from payments.payables import Payable, payables
-from registrations.models import Entry, Registration, Renewal
+from registrations.models import Registration, Renewal
 
 
-class EntryPayable(Payable):
+class _EntryPayable(Payable):
+    """Abstract parent class for Registration- and RenewalPayables."""
+
     @property
     def payment_amount(self):
         return self.model.contribution
@@ -17,15 +19,8 @@ class EntryPayable(Payable):
         return None
 
     @property
-    def payment_topic(self):
-        return "Registration entry"
-
-    @property
     def payment_notes(self):
         return f"{self.payment_topic}. Creation date: {date(self.model.created_at)}. Completion date: {date(self.model.updated_at)}"
-
-    def can_manage_payment(self, member):
-        return member and member.has_perm("registrations.change_entry")
 
     @classproperty
     def immutable_after_payment(self):
@@ -36,7 +31,7 @@ class EntryPayable(Payable):
         return ["length", "contribution"]
 
 
-class RegistrationPayable(EntryPayable):
+class RegistrationPayable(_EntryPayable):
     @property
     def payment_topic(self):
         return f"Membership registration {self.model.membership_type} ({self.model.length})"
@@ -45,7 +40,7 @@ class RegistrationPayable(EntryPayable):
         return member and member.has_perm("registrations.change_registration")
 
 
-class RenewalPayable(EntryPayable):
+class RenewalPayable(_EntryPayable):
     @property
     def payment_payer(self):
         return self.model.member
@@ -59,6 +54,5 @@ class RenewalPayable(EntryPayable):
 
 
 def register():
-    payables.register(Entry, EntryPayable)
     payables.register(Renewal, RenewalPayable)
     payables.register(Registration, RegistrationPayable)
