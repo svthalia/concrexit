1fd71df2e59ccad997cf9e06a7d8160b3eedfb04
movieminer <thijsg.dejong@gmail.com>
2023-10-18 22:41:33 +0200

    Slightly improved admins

--- website/moneybirdsynchronization/admin.py
+++ website/moneybirdsynchronization/admin.py
@@ -7,14 +7,68 @@ from .models import MoneybirdContact, MoneybirdExternalInvoice, MoneybirdPayment
 class MoneybirdContactAdmin(admin.ModelAdmin):
     """Manage moneybird contacts."""
 
-    list_display = ("member", "moneybird_id")
+    list_display = (
+        "member",
+        "moneybird_id",
+        "moneybird_sepa_mandate_id",
+    )
+
+    fields = (
+        "member",
+        "moneybird_id",
+        "moneybird_sepa_mandate_id",
+    )
+
+    readonly_fields = ("member",)
+
+    search_fields = ("member__first_name", "member__last_name", "moneybird_id")
 
 
 @admin.register(MoneybirdExternalInvoice)
 class MoneybirdExternalInvoiceAdmin(admin.ModelAdmin):
-    list_display = ("payable_object",)
+    """Manage moneybird external invoices."""
+
+    list_display = (
+        "payable_object",
+        "payable_model",
+        "moneybird_invoice_id",
+    )
+
+    fields = (
+        "payable_object",
+        "payable_model",
+        "moneybird_invoice_id",
+    )
+
+    readonly_fields = ("payable_object",)
+
+    search_fields = (
+        "payable_model__app_label",
+        "payable_model__model",
+        "moneybird_invoice_id",
+    )
 
 
 @admin.register(MoneybirdPayment)
 class MoneybirdPaymentAdmin(admin.ModelAdmin):
-    list_display = ("payment",)
+    """Manage moneybird payments."""
+
+    list_display = (
+        "payment",
+        "moneybird_financial_statement_id",
+        "moneybird_financial_mutation_id",
+    )
+
+    fields = (
+        "payment",
+        "moneybird_financial_statement_id",
+        "moneybird_financial_mutation_id",
+    )
+
+    readonly_fields = ("payment",)
+
+    search_fields = (
+        "payment__amount",
+        "moneybird_financial_mutation_id",
+        "moneybird_financial_statement_id",
+    )
