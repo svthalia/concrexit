3fa6155ae7b47bf24ad5e046af1a47412ed69d5c
Nienke <n.wessel@xs4all.nl>
2023-10-29 15:40:54 +0100

    Show random picture in header on photos page

--- website/thaliawebsite/templatetags/pick_header_image.py
+++ website/thaliawebsite/templatetags/pick_header_image.py
@@ -9,13 +9,29 @@ register = template.Library()
 HEADERS = [
     "img/headers/banner_default.jpg",
     "img/headers/banner2.jpg",
-    "img/headers/banner4.jpg",
     "img/headers/banner5.jpg",
     "img/headers/banner6.jpg",
+    "img/headers/banner_new_Huygens.png",
+    "img/headers/banner_new_Huygenstent.png",
+    "img/headers/banner_new_robot.png",
+    "img/headers/banner_new_tent.png",
+]
+
+HEADERS_FUN = [
+    "img/headers/banner_new_wine.png",
+    "img/headers/banner_new_winetasting.png",
+    "img/headers/banner_new_christmas.png",
+    "img/headers/banner_new_Huygenstent.png",
+    "img/headers/banner_new_bingo.png",
+    "img/headers/banner_new_food.png",
 ]
 
 
 @register.simple_tag
-def pick_header_image():
+def pick_header_image(type="normal"):
     """Render a random header image."""
-    return staticfiles_storage.url(random.choice(HEADERS))
+    if type == "fun":
+        headers = HEADERS_FUN
+    else:
+        headers = HEADERS
+    return staticfiles_storage.url(random.choice(headers))
