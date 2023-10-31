7604dde6355ae89fcc8e3b8d3880d58ad2fdd50a
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-10-31 11:52:45 +0100

    Use img srcset attribute for multiple resolutions

--- website/thaliawebsite/templatetags/pick_header_image.py
+++ website/thaliawebsite/templatetags/pick_header_image.py
@@ -3,27 +3,28 @@ import random
 
 from django import template
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 HEADERS = [
-    "img/headers/banner_default.jpg",
-    "img/headers/banner2.jpg",
-    "img/headers/banner5.jpg",
-    "img/headers/banner6.jpg",
-    "img/headers/banner_new_Huygens.png",
-    "img/headers/banner_new_Huygenstent.png",
-    "img/headers/banner_new_robot.png",
-    "img/headers/banner_new_tent.png",
+    "img/headers/banner_default",
+    "img/headers/banner_bicycles",
+    "img/headers/banner_huygenshall",
+    "img/headers/banner_huygensfront",
+    "img/headers/banner_huygens",
+    "img/headers/banner_huygenstent",
+    "img/headers/banner_robot",
+    "img/headers/banner_tent",
 ]
 
 HEADERS_FUN = [
-    "img/headers/banner_new_wine.png",
-    "img/headers/banner_new_winetasting.png",
-    "img/headers/banner_new_christmas.png",
-    "img/headers/banner_new_Huygenstent.png",
-    "img/headers/banner_new_bingo.png",
-    "img/headers/banner_new_food.png",
+    "img/headers/banner_wine",
+    "img/headers/banner_winetasting",
+    "img/headers/banner_christmas",
+    "img/headers/banner_huygenstent",
+    "img/headers/banner_bingo",
+    "img/headers/banner_food",
 ]
 
 
@@ -34,4 +35,8 @@ def pick_header_image(type="normal"):
         headers = HEADERS_FUN
     else:
         headers = HEADERS
-    return staticfiles_storage.url(random.choice(headers))
+    header = random.choice(headers)
+    header_2k = staticfiles_storage.url(header + "-2k.webp")
+    header_5k = staticfiles_storage.url(header + "-5k.webp")
+
+    return mark_safe(f"{header_2k}, {header_5k} 3x")
