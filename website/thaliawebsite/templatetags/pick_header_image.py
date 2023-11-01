02198da9ea96819c7c425b2d1febf5fc9861bacd
NienkeWessel <36206600+NienkeWessel@users.noreply.github.com>
2023-11-01 14:16:07 +0100

    Update header photos (#3462)
    * New header images
    
    * Show random picture in header on photos page
    
    * Remove ugly header
    
    * Convert banners to WebP
    
    * Use img srcset attribute for multiple resolutions
    
    * Fix default `random` getting deterministic seed
    
    This seemed to mess with random header picking on the albums list,
    where the same random image seemed to be getting picked forever after
    reloading once.
    It's just a hunch, but this seemed to fix it as far as I can tell.
    
    ---------
    
    Co-authored-by: Dirk Doesburg <dirk.doesburg@gmail.com>

--- website/thaliawebsite/templatetags/pick_header_image.py
+++ website/thaliawebsite/templatetags/pick_header_image.py
@@ -3,19 +3,40 @@ import random
 
 from django import template
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 HEADERS = [
-    "img/headers/banner_default.jpg",
-    "img/headers/banner2.jpg",
-    "img/headers/banner4.jpg",
-    "img/headers/banner5.jpg",
-    "img/headers/banner6.jpg",
+    "img/headers/banner_default",
+    "img/headers/banner_bicycles",
+    "img/headers/banner_huygenshall",
+    "img/headers/banner_huygensfront",
+    "img/headers/banner_huygens",
+    "img/headers/banner_huygenstent",
+    "img/headers/banner_robot",
+    "img/headers/banner_tent",
+]
+
+HEADERS_FUN = [
+    "img/headers/banner_wine",
+    "img/headers/banner_winetasting",
+    "img/headers/banner_christmas",
+    "img/headers/banner_huygenstent",
+    "img/headers/banner_bingo",
+    "img/headers/banner_food",
 ]
 
 
 @register.simple_tag
-def pick_header_image():
+def pick_header_image(type="normal"):
     """Render a random header image."""
-    return staticfiles_storage.url(random.choice(HEADERS))
+    if type == "fun":
+        headers = HEADERS_FUN
+    else:
+        headers = HEADERS
+    header = random.choice(headers)
+    header_2k = staticfiles_storage.url(header + "-2k.webp")
+    header_5k = staticfiles_storage.url(header + "-5k.webp")
+
+    return mark_safe(f"{header_2k}, {header_5k} 3x")
