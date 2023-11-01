a2468018f208c565742d315ec92f8724b4bab05b
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-11-01 15:13:14 +0100

    Test payable immutability

--- website/registrations/tests/test_payables.py
+++ website/registrations/tests/test_payables.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 from unittest.mock import MagicMock
 
+from django.conf import settings
 from django.test import TestCase
+from django.utils import timezone
 
 from members.models import Membership
+from members.models.member import Member
+from payments.exceptions import PaymentError
+from payments.models import Payment
+from payments.payables import payables
+from payments.services import create_payment
 from registrations.models import Registration, Renewal
 from registrations.payables import RegistrationPayable, RenewalPayable
 
@@ -35,6 +42,8 @@ class RenewalPayableTest(TestCase):
 
 
 class RegistrationPayableTest(TestCase):
+    fixtures = ["members.json"]
+
     def setUp(self):
         self.model = MagicMock()
         self.model.contribution = 20
@@ -60,3 +69,31 @@ class RegistrationPayableTest(TestCase):
             "Membership registration benefactor (year). Creation date: March 4, 2018. Completion date: March 5, 2018",
         )
         self.assertFalse(self.payable.can_manage_payment(None))
+
+    def test_immutable_fields_after_payment(self):
+        payables.register(Registration, RegistrationPayable)
+        registration = Registration.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="johndoe@example.com",
+            programme="computingscience",
+            starting_year=2014,
+            address_street="Heyendaalseweg 135",
+            address_street2="",
+            address_postal_code="6525AJ",
+            address_city="Nijmegen",
+            address_country="NL",
+            phone_number="06123456789",
+            birthday=timezone.now().replace(year=1990),
+            length=Registration.MEMBERSHIP_YEAR,
+            membership_type=Membership.MEMBER,
+            status=Registration.STATUS_ACCEPTED,
+            contribution=settings.MEMBERSHIP_PRICES[Registration.MEMBERSHIP_YEAR],
+        )
+
+        create_payment(registration, Member.objects.get(pk=1), Payment.CASH)
+
+        registration.contribution = 0
+
+        with self.assertRaises(PaymentError):
+            registration.save()
