5b36e1956ca7053c8707ba4afa87f48fd49f54fb
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-11-09 20:35:46 +0100

    Fix formatting

--- website/members/api/v2/permissions.py
+++ website/members/api/v2/permissions.py
@@ -3,5 +3,6 @@ from rest_framework.permissions import BasePermission
 
 class HasActiveMembership(BasePermission):
     """DRF permission equivalent of the `@membership_required` view decorator."""
+
     def has_permission(self, request, view):
         return request.member and request.member.has_active_membership()
