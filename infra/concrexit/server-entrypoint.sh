367a0804f478a1283d7a0b318dce7ff34173f822
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-11-09 18:51:44 +0100

    Run collectstatic only once per container

--- infra/concrexit/server-entrypoint.sh
+++ infra/concrexit/server-entrypoint.sh
@@ -3,11 +3,13 @@
 chown -R appuser /volumes/static
 chown -R appuser /volumes/media
 
-MANAGE_PY=1 runuser -u appuser -- ./manage.py collectstatic --no-input
+if [ ! -f /app/.collectstatic_done ]; then # Only run collectstatic if it hasn't been run before in this container.s
+    MANAGE_PY=1 runuser -u appuser -- ./manage.py collectstatic --no-input && touch /app/.collectstatic_done
+fi
 MANAGE_PY=1 runuser -u appuser -- ./manage.py migrate --no-input
 MANAGE_PY=1 runuser -u appuser -- ./manage.py createcachetable
 
 
 exec runuser -u appuser -- /venv/bin/gunicorn \
-  --config=/app/gunicorn.conf.py \
+  --config /app/gunicorn.conf.py \
   thaliawebsite.wsgi:application
