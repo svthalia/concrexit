b8095faf77cbe5ba5e0ca6fd01526631b0574191
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-11-11 11:29:18 +0100

    Fill in some tests

--- website/registrations/tests/test_services.py
+++ website/registrations/tests/test_services.py
@@ -1,8 +1,10 @@
 from django.core import mail
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.utils import timezone
 
 from members.models import Member, Membership
+from payments.models import BankAccount, Payment
+from payments.services import create_payment
 from registrations import services
 from registrations.models import Entry, Registration
 
@@ -79,21 +81,78 @@ class ServicesTest(TestCase):
         self.member_registration.status = Entry.STATUS_REVIEW
         self.member_registration.save()
 
+        # Existing member has the default username.
+        self.member.username = "jdoe"
+        self.member.save()
+
         with self.subTest("Username not unique"):
-            raise NotImplementedError
-            # Raises, and does not commit any changes.
+            # Raises and does not commit changes. Does not send email.
+            with self.assertRaises(ValueError):
+                services.accept_registration(self.member_registration, actor=self.admin)
+
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_REVIEW)
+            self.assertEqual(len(mail.outbox), 0)
+
+        self.member_registration.username = "johndoe"
+        self.member_registration.save()
+
+        self.member.email = self.member_registration.email
+        self.member.save()
+        mail.outbox = []
 
         with self.subTest("Email not unique"):
-            raise NotImplementedError
-            # Raises, and does not commit any changes.
+            # Raises and does not commit changes. Does not send email.
+            with self.assertRaises(ValueError):
+                services.accept_registration(self.member_registration, actor=self.admin)
+
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_REVIEW)
+            self.assertEqual(len(mail.outbox), 0)
+
+        self.member_registration.email = "unique@example.com"
+        self.member_registration.save()
+        mail.outbox = []
 
         with self.subTest("Normal"):
-            raise NotImplementedError
-            # Changes status and sends email.
+            # Succeeds and sends payment email.
+            services.accept_registration(self.member_registration, actor=self.admin)
+
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_ACCEPTED)
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(
+                mail.outbox[0].subject,
+                "[THALIA] Registration accepted",
+            )
+
+        services.revert_registration(self.member_registration, actor=self.admin)
+
+        self.member_registration.direct_debit = True
+        self.member_registration.iban = "NL12ABNA1234567890"
+        self.member_registration.signature = "base64,png"
+        self.member_registration.initials = "J."
+        self.member_registration.save()
+        mail.outbox = []
 
         with self.subTest("With Thalia Pay"):
-            raise NotImplementedError
-            # Completes, does not send payment email.
+            # Completes the registration, does not send payment email,
+            # but sends final email after completing the registration.
+            services.accept_registration(self.member_registration, actor=self.admin)
+
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_COMPLETED)
+            self.assertIsNotNone(self.member_registration.membership)
+            self.assertIsNotNone(self.member_registration.membership.user)
+            member = self.member_registration.membership.user
+            self.assertEqual(BankAccount.objects.filter(owner_id=member.id).count(), 1)
+            self.assertEqual(self.member_registration.payment.amount, 7.5)
+            self.assertEqual(self.member_registration.payment.type, Payment.TPAY)
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(
+                mail.outbox[0].subject,
+                "[THALIA] Welcome to Study Association Thalia",
+            )
 
     def test_reject_registration(self):
         self.member_registration.status = Entry.STATUS_REVIEW
@@ -104,10 +163,56 @@ class ServicesTest(TestCase):
         self.assertEqual(len(mail.outbox), 1)
 
     def test_revert_registration(self):
-        raise NotImplementedError
+        self.member_registration.status = Entry.STATUS_REJECTED
+        self.member_registration.save()
+        with self.subTest("Revert rejected registration."):
+            services.revert_registration(self.member_registration, actor=self.admin)
+            self.assertEqual(self.member_registration.status, Entry.STATUS_REVIEW)
+
+        self.member_registration.status = Entry.STATUS_ACCEPTED
+        self.member_registration.save()
+
+        with override_settings(SUSPEND_SIGNALS=True):
+            payment = create_payment(self.member_registration, self.admin, Payment.CASH)
+            # Signals are suspended, so this does not complete the registration.
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_ACCEPTED)
+
+        with self.subTest("Revert paid, but somehow not completed registration."):
+            # This case is similar to how a registration is automatically reverted when
+            # an exception is raised in complete_registration() in a signal handler.
+            services.revert_registration(self.member_registration, actor=self.admin)
+            self.assertEqual(self.member_registration.status, Entry.STATUS_REVIEW)
+            self.assertFalse(Payment.objects.filter(pk=payment.pk).exists())
 
     def test_complete_registration(self):
-        raise NotImplementedError
+        self.member_registration.status = Entry.STATUS_ACCEPTED
+        self.member_registration.save()
+
+        with self.subTest("Complete member registration when payment is made."):
+            # Signal triggers call to complete_registration.
+            create_payment(self.member_registration, self.admin, Payment.CASH)
+
+            self.member_registration.refresh_from_db()
+            self.assertEqual(self.member_registration.status, Entry.STATUS_COMPLETED)
+            membership = self.member_registration.membership
+            self.assertIsNotNone(membership)
+            member = membership.user
+            self.assertEqual(member.first_name, "John")
+            self.assertEqual(member.last_name, "Doe")
+            self.assertEqual(member.username, "jdoe")
+            # TODO: check membership since and until.
+            # TODO: check bank account.
+            # TODO: check sent emails.
+
+        with self.subTest("Complete benefactor registration when payment is made."):
+            raise NotImplementedError
+
+        with self.subTest("Username is not unique."):
+            raise NotImplementedError
+
+        with self.subTest("Email is not unique."):
+            raise NotImplementedError
 
     def test_accept_renewal(self):
         raise NotImplementedError
