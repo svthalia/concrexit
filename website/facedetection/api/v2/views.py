fd1839fc29c71c8514a742d2ad7a1af92e2e484c
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-12-16 18:38:38 +0100

    Add /photos/facedetection/matches

--- website/facedetection/api/v2/views.py
+++ website/facedetection/api/v2/views.py
@@ -1,14 +1,70 @@
 from django.conf import settings
+from django.db.models import Count, Q
 from django.utils import timezone
 
 from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.generics import DestroyAPIView, ListCreateAPIView
+from rest_framework.generics import DestroyAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.schemas.openapi import AutoSchema
 
+from photos.api.v2.serializers.photo import PhotoListSerializer
+from photos.models import Photo
 from thaliawebsite.api.v2.permissions import IsAuthenticatedOrTokenHasScopeForMethod
 from utils.media.services import fetch_thumbnails
 
 from .serializers import ReferenceFaceSerializer
+
+
+class YourPhotosView(ListAPIView):
+    serializer_class = PhotoListSerializer
+    permission_classes = [IsAuthenticatedOrTokenHasScope]
+    required_scopes = ["photos:read", "facedetection:read"]
+
+    schema = AutoSchema(operation_id_base="FacedetectionMatches")
+
+    def get(self, request, *args, **kwargs):
+        if not request.member or request.member.current_membership is None:
+            raise PermissionDenied(
+                detail="You need to be a member in order to view your facedetection photos."
+            )
+        return self.list(request, *args, **kwargs)
+
+    def get_serializer(self, *args, **kwargs):
+        if len(args) > 0:
+            photos = args[0]
+            fetch_thumbnails([photo.file for photo in photos])
+        return super().get_serializer(*args, **kwargs)
+
+    def get_queryset(self):
+        member = self.request.member
+
+        reference_faces = member.reference_faces.filter(
+            marked_for_deletion_at__isnull=True,
+        )
+
+        # Filter out matches from long before the member's first membership.
+        albums_since = member.earliest_membership.since - timezone.timedelta(days=31)
+        photos = Photo.objects.select_related("album").filter(
+            album__date__gte=albums_since
+        )
+
+        # Filter out matches from after the member's last membership.
+        if member.latest_membership.until is not None:
+            photos = photos.filter(album__date__lte=member.latest_membership.until)
+
+        # Actually match the reference faces.
+        photos = photos.filter(album__hidden=False, album__is_processing=False).filter(
+            facedetectionphoto__encodings__matches__reference__in=reference_faces,
+        )
+
+        return (
+            photos.annotate(
+                member_likes=Count("likes", filter=Q(likes__member=self.request.member))
+            )
+            .select_properties("num_likes")
+            .select_properties("num_likes")
+            .order_by("-album__date", "-pk")
+        )
 
 
 class ReferenceFaceListView(ListCreateAPIView):
