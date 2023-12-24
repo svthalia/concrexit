68219ff9c596c8782ab6ce92332ddb3e37b0160e
Jaap Aarts <JAicewizard@users.noreply.github.com>
2023-12-24 22:06:56 +0100

    Add connection pooling to postgresql (#3545)

--- website/thaliawebsite/settings.py
+++ website/thaliawebsite/settings.py
@@ -468,6 +468,7 @@ if DATABASE_ENGINE == "postgresql":
             "NAME": os.environ.get("POSTGRES_DB", ""),
             "HOST": os.environ.get("POSTGRES_HOST", ""),
             "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+            "CONN_MAX_AGE": 300,
         }
     }
 
