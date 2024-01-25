7f8a545e1ca7ac0194af8cf1ad64b42d46e9c7dc
thijmengg <146195501+thijmengg@users.noreply.github.com>
2024-01-25 19:09:14 +0100

    Update merchandise pages layout (#3484)
    * This are the changes to the front-end merchandise page
    
    * Use bootstrap on merch list page
    
    * Bootstrapify product page
    
    ---------
    
    Co-authored-by: Dirk Doesburg <dirk.doesburg@gmail.com>

--- website/merchandise/views.py
+++ website/merchandise/views.py
@@ -1,4 +1,5 @@
 """The views for the merchandise package."""
+from django.http import Http404
 from django.shortcuts import render
 
 from merchandise.models import MerchandiseItem
@@ -13,3 +14,14 @@ def index(request):
     items = MerchandiseItem.objects.all()
 
     return render(request, "merchandise/index.html", {"items": items})
+
+
+def product_page(request, id):
+    try:
+        product = MerchandiseItem.objects.get(pk=id)
+    except MerchandiseItem.DoesNotExist:
+        raise Http404(
+            "This item may not exists, or is removed. Please check if the link is correct!"
+        )
+
+    return render(request, "merchandise/product_page.html", {"product": product})
