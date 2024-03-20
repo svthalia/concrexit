5f68b8e3f1ea5a5bf423718c6bbf8e67ba96b6fd
Quinten Kock <quinten@quinten.space>
2024-03-20 23:31:47 +0100

    Remove pointless 'event' field from NewsletterEventForm

--- website/newsletters/forms.py
+++ website/newsletters/forms.py
@@ -2,7 +2,6 @@
 
 from django import forms
 from django.contrib.admin.widgets import AdminDateWidget
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from events.models import Event
@@ -14,21 +13,9 @@ from .models import Newsletter, NewsletterEvent
 class NewsletterEventForm(forms.ModelForm):
     """Custom ModelForm for the NewsletterEvent model to add the order field and javascript for automatic field filling."""
 
-    event = forms.ChoiceField(label=_("Event"))
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields["event"].choices = [(None, "-----")] + [
-            (e.pk, e.title)
-            for e in Event.objects.filter(published=True, start__gt=timezone.now())
-        ]
-        self.fields["event"].required = False
-
     class Meta:
         fields = (
             "order",
-            "event",
             "title",
             "url",
             "description",
