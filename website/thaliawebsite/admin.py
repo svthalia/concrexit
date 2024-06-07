a65e7c3a5eea745feb75aa2e0fc4afa5182fd491
Ties Dirksen <78494266+T8902@users.noreply.github.com>
2024-06-07 15:04:30 +0200

    Add 2FA requirement for adminpage (#3722)

--- website/thaliawebsite/admin.py
+++ website/thaliawebsite/admin.py
@@ -1,6 +1,17 @@
 """Settings for the admin site."""
+
+from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-admin.site.site_header = _("Thalia administration")
-admin.site.site_title = _("Thalia")
+from django_otp import user_has_device
+
+
+class ThaliaAdminSite(admin.AdminSite):
+    site_header = _("Thalia administration")
+    site_title = _("Thalia")
+
+    def has_permission(self, request):
+        return super().has_permission(request) and (
+            settings.DEBUG or user_has_device(request.user)
+        )
