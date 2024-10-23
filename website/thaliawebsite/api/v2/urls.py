8ec4d0c51c084eea88331a61ba182264eedd25e6
annalorenz123 <annalorenz@gmail.com>
2024-10-23 19:58:00 +0200

    Removed unnecessary line

--- website/thaliawebsite/api/v2/urls.py
+++ website/thaliawebsite/api/v2/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path("", include("pizzas.api.v2.urls")),
     path("", include("pushnotifications.api.v2.urls")),
     path("", include("sales.api.v2.urls")),
-    path("", include("sales.api.v2.urls")),
     path(
         "schema",
         get_schema_view(
