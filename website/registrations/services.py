9cf04409bb3bde1db0384bf9938b5b5a726715d4
Ties Dirksen <tiesdirksen@gmail.com>
2024-10-31 15:43:54 +0100

    Fix some logical mistakes made

--- website/registrations/services.py
+++ website/registrations/services.py
@@ -252,19 +252,17 @@ def complete_renewal(renewal: Renewal):
     if since.month == 8:
         lecture_year += 1
 
+    latest_membership = member.latest_membership
     current_membership = member.current_membership
     if current_membership is not None and current_membership.until is None:
         raise ValueError("This member already has a never ending membership")
     until = timezone.datetime(year=lecture_year + 1, month=9, day=1).date()
     with transaction.atomic():
-        if renewal.membership_type == Renewal.MEMBERSHIP_STUDY:
-            # Handle the 'normal' non-(discounted)-upgrade case.
-            renewal.membership = Membership.objects.create(
-                type=renewal.membership_type,
-                user=member,
-                since=since,
-                until=until,
-            )
+        if renewal.length == Renewal.MEMBERSHIP_STUDY:
+            latest_membership.study_long = True
+            latest_membership.until = until
+            latest_membership.save()
+            renewal.membership = latest_membership
         else:
             if current_membership:
                 since = current_membership.until
