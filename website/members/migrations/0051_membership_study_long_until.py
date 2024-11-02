184d6abb5f474e091be2d11672a9f3f78526d4cb
Ties Dirksen <tiesdirksen@gmail.com>
2024-11-02 15:53:26 +0100

    Fix migration 0051

--- website/members/migrations/0051_membership_study_long_until.py
+++ website/members/migrations/0051_membership_study_long_until.py
@@ -5,13 +5,14 @@ from django.db import migrations, models
 
 def change_until(apps, schema_editor):
     Membership = apps.get_model("members", "Membership")
-    members = Membership.objects.filter(until=None and __.length == __.MEMBERSHIP_STUDY)
-    for member in members:
-        member.study_long = True
-        member.until = datetime.datetime(
-            year=timezone.now().year, month=9, day=1
-        ) + datetime.timedelta(days=365)
-        member.save()
+
+    new_until = datetime.datetime(
+        year=timezone.now().year, month=9, day=1
+    ) + datetime.timedelta(days=365)
+
+    Membership.objects.filter(until=None, type=Membership.MEMBER).update(
+        study_long=True, until=new_until.date()
+    )
 
 
 class Migration(migrations.Migration):
