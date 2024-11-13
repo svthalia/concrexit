2bcfbc6df7107825fad4b42e12c57da0e7486e66
Quinten Kock <quintenkock@gmail.com>
2024-11-14 00:02:10 +0100

    Add pk to serializer

--- website/announcements/api/v2/serializers.py
+++ website/announcements/api/v2/serializers.py
@@ -1,5 +1,6 @@
-from announcements.models import Slide
 from rest_framework import serializers
+
+from announcements.models import Slide
 from thaliawebsite.api.v2.serializers import CleanedHTMLSerializer, ThumbnailSerializer
 from thaliawebsite.api.v2.serializers.cleaned_model_serializer import (
     CleanedModelSerializer,
@@ -10,6 +11,7 @@ class AnnouncementSerializer(serializers.Serializer):
     content = CleanedHTMLSerializer(read_only=True)
     closeable = serializers.BooleanField(read_only=True)
     icon = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True, default=None)
 
 
 class SlideSerializer(CleanedModelSerializer):
