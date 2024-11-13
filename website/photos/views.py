8ad1ceeeaad69ff639a43d862612adbfc9266156
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-11-13 20:00:10 +0100

    Format

--- website/photos/views.py
+++ website/photos/views.py
@@ -47,12 +47,12 @@ class IndexView(LoginRequiredMixin, PagedView):
         context["keywords"] = self.keywords
         fetch_thumbnails([x.cover.file for x in context["object_list"] if x.cover])
 
-        context[
-            "has_rejected_reference_faces"
-        ] = self.request.member.reference_faces.filter(
-            status=ReferenceFace.Status.REJECTED,
-            marked_for_deletion_at__isnull=True,
-        ).exists()
+        context["has_rejected_reference_faces"] = (
+            self.request.member.reference_faces.filter(
+                status=ReferenceFace.Status.REJECTED,
+                marked_for_deletion_at__isnull=True,
+            ).exists()
+        )
 
         context["has_reference_faces"] = self.request.member.reference_faces.filter(
             marked_for_deletion_at__isnull=True
