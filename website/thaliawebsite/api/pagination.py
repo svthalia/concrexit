18575cf888127b81d52eb3b949952d4992747678
Ties Dirksen <tiesdirksen@gmail.com>
2024-11-13 19:16:41 +0100

    Remove API v1

--- website/thaliawebsite/api/pagination.py
+++ website/thaliawebsite/api/pagination.py
@@ -3,12 +3,10 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class APIv2LimitOffsetPagination(LimitOffsetPagination):
-    """Pagination class that uses LimitOffsetPagination and sets the default value for the pagination size to None for API v1."""
+    """Pagination class that uses LimitOffsetPagination."""
 
     def get_limit(self, request):
         if self.limit_query_param in request.query_params:
             return super().get_limit(request)
 
-        if request.version == "v1":
-            return None
         return self.default_limit
