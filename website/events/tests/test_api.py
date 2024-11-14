f9c5d4887d94ac05c185a564e40faa03a7cf8362
Ties Dirksen <tiesdirksen@gmail.com>
2024-11-14 13:15:28 +0100

    Add some tests

--- website/events/tests/test_api.py
+++ website/events/tests/test_api.py
@@ -1,22 +1,13 @@
 import datetime
 
+from activemembers.models import Committee
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils import timezone
-
-from rest_framework.test import APIClient
-
-from activemembers.models import Committee
-from events.models import (
-    BooleanRegistrationInformation,
-    Event,
-    EventRegistration,
-    IntegerRegistrationInformation,
-    RegistrationInformationField,
-    TextRegistrationInformation,
-)
+from events.models import Event, EventRegistration
 from events.models.external_event import ExternalEvent
 from members.models import Member
+from rest_framework.test import APIClient
 
 
 @override_settings(SUSPEND_SIGNALS=True)
@@ -53,317 +44,6 @@ class RegistrationApiTest(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.client.force_login(self.member)
-
-    def test_registration_register_not_required(self):
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(self.event.participants.count(), 1)
-
-    def test_registration_register(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-        self.assertEqual(self.event.participants.count(), 1)
-        self.assertEqual(self.event.eventregistration_set.first().member, self.member)
-
-    def test_registration_register_twice(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 403)
-        self.assertEqual(self.event.participants.count(), 1)
-
-    def test_registration_register_closed(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=2)
-        self.event.registration_end = timezone.now() - datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 403)
-        self.assertEqual(self.event.participants.count(), 0)
-
-    def test_registration_cancel(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-        reg = EventRegistration.objects.create(event=self.event, member=self.member)
-        response = self.client.delete(f"/api/v1/registrations/{reg.pk}/", follow=True)
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(self.event.participants.count(), 0)
-
-    def test_registration_register_no_fields(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-
-        field1 = RegistrationInformationField.objects.create(
-            pk=1,
-            event=self.event,
-            type=RegistrationInformationField.BOOLEAN_FIELD,
-            name="test bool",
-            required=False,
-        )
-
-        field2 = RegistrationInformationField.objects.create(
-            pk=2,
-            event=self.event,
-            type=RegistrationInformationField.INTEGER_FIELD,
-            name="test int",
-            required=False,
-        )
-
-        field3 = RegistrationInformationField.objects.create(
-            pk=3,
-            event=self.event,
-            type=RegistrationInformationField.TEXT_FIELD,
-            name="test text",
-            required=False,
-        )
-
-        response = self.client.post(
-            "/api/v1/events/1/registrations/",
-            {"info_field_1": True, "info_field_2": 42, "info_field_3": "text"},
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-
-        self.assertEqual(self.event.participants.count(), 1)
-        registration = self.event.eventregistration_set.first()
-        self.assertEqual(field1.get_value_for(registration), None)
-        self.assertEqual(field2.get_value_for(registration), None)
-        self.assertEqual(field3.get_value_for(registration), None)
-
-    def test_registration_missing_fields(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-
-        RegistrationInformationField.objects.create(
-            pk=1,
-            event=self.event,
-            type=RegistrationInformationField.BOOLEAN_FIELD,
-            name="test bool",
-            required=False,
-        )
-
-        RegistrationInformationField.objects.create(
-            pk=2,
-            event=self.event,
-            type=RegistrationInformationField.INTEGER_FIELD,
-            name="test int",
-            required=False,
-        )
-
-        RegistrationInformationField.objects.create(
-            pk=3,
-            event=self.event,
-            type=RegistrationInformationField.TEXT_FIELD,
-            name="test text",
-            required=False,
-        )
-
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-        self.assertEqual(self.event.participants.count(), 1)
-
-    def test_registration_register_fields_required(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-
-        RegistrationInformationField.objects.create(
-            event=self.event,
-            type=RegistrationInformationField.TEXT_FIELD,
-            name="test",
-            required=True,
-        )
-
-        response = self.client.post("/api/v1/events/1/registrations/", follow=True)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-        self.assertEqual(self.event.participants.count(), 1)
-
-    def test_registration_update_form_load_not_changes_fields(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-
-        field1 = RegistrationInformationField.objects.create(
-            pk=1,
-            event=self.event,
-            type=RegistrationInformationField.BOOLEAN_FIELD,
-            name="test bool",
-            required=False,
-        )
-
-        field2 = RegistrationInformationField.objects.create(
-            pk=2,
-            event=self.event,
-            type=RegistrationInformationField.INTEGER_FIELD,
-            name="test int",
-            required=False,
-        )
-
-        field3 = RegistrationInformationField.objects.create(
-            pk=3,
-            event=self.event,
-            type=RegistrationInformationField.TEXT_FIELD,
-            name="test text",
-            required=False,
-        )
-
-        registration = EventRegistration.objects.create(
-            event=self.event, member=self.member
-        )
-        BooleanRegistrationInformation.objects.create(
-            registration=registration, field=field1, value=True
-        )
-        IntegerRegistrationInformation.objects.create(
-            registration=registration, field=field2, value=42
-        )
-        TextRegistrationInformation.objects.create(
-            registration=registration, field=field3, value="text"
-        )
-
-        # as if there is a csrf token
-        response = self.client.get(
-            f"/api/v1/registrations/{registration.pk}/", follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["member"], self.member.pk)
-
-        registration = self.event.eventregistration_set.first()
-        self.assertEqual(field1.get_value_for(registration), True)
-        self.assertEqual(field2.get_value_for(registration), 42)
-        self.assertEqual(field3.get_value_for(registration), "text")
-
-    def test_registration_update_form_post_changes_fields(self):
-        self.event.registration_start = timezone.now() - datetime.timedelta(hours=1)
-        self.event.registration_end = timezone.now() + datetime.timedelta(hours=1)
-        self.event.cancel_deadline = timezone.now() + datetime.timedelta(hours=1)
-        self.event.save()
-
-        field1 = RegistrationInformationField.objects.create(
-            pk=1,
-            event=self.event,
-            type=RegistrationInformationField.BOOLEAN_FIELD,
-            name="test bool",
-            required=False,
-        )
-
-        field2 = RegistrationInformationField.objects.create(
-            pk=2,
-            event=self.event,
-            type=RegistrationInformationField.INTEGER_FIELD,
-            name="test int",
-            required=False,
-        )
-
-        field3 = RegistrationInformationField.objects.create(
-            pk=3,
-            event=self.event,
-            type=RegistrationInformationField.TEXT_FIELD,
-            name="test text",
-            required=False,
-        )
-
-        response = self.client.post(
-            "/api/v1/events/1/registrations/",
-            {
-                "fields[info_field_1]": False,
-                "fields[info_field_2": 42,
-                "fields[info_field_3]": "text",
-                "csrf": "random",
-            },
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["member"], self.member.pk)
-
-        registration = EventRegistration.objects.get(
-            event=self.event, member=self.member
-        )
-        self.assertEqual(field1.get_value_for(registration), None)
-        self.assertEqual(field2.get_value_for(registration), None)
-        self.assertEqual(field3.get_value_for(registration), None)
-
-        response = self.client.patch(
-            f"/api/v1/registrations/{registration.pk}/",
-            {
-                "fields[info_field_1]": True,
-                "fields[info_field_2]": 1337,
-                "fields[info_field_3]": "no text",
-                "csrf": "random",
-            },
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["member"], self.member.pk)
-
-        self.assertEqual(self.event.participants.count(), 1)
-        registration = self.event.eventregistration_set.first()
-        self.assertEqual(field1.get_value_for(registration), True)
-        self.assertEqual(field2.get_value_for(registration), 1337)
-        self.assertEqual(field3.get_value_for(registration), "no text")
-
-    def test_registration_organiser(self):
-        reg0 = EventRegistration.objects.create(event=self.event, member=self.member)
-        reg1 = EventRegistration.objects.create(event=self.event, name="Test 1")
-        reg2 = EventRegistration.objects.create(event=self.event, name="Test 2")
-
-        self.member.is_superuser = True
-        self.member.save()
-
-        response = self.client.patch(
-            f"/api/v1/registrations/{reg0.pk}/",
-            {"csrf": "random", "present": True, "payment": "cash_payment"},
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["member"], self.member.pk)
-        reg0.refresh_from_db()
-        self.assertIsNotNone(reg0.payment_id)
-        self.assertTrue(reg0.present)
-
-        response = self.client.patch(
-            f"/api/v1/registrations/{reg1.pk}/",
-            {"csrf": "random", "present": True, "payment": "card_payment"},
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["member"], None)
-        self.assertEqual(response.data["name"], "Test 1")
-        reg1.refresh_from_db()
-        self.assertEqual(reg1.payment.type, "card_payment")
-        self.assertTrue(reg1.present)
-
-        response = self.client.patch(
-            f"/api/v1/registrations/{reg2.pk}/",
-            {"csrf": "random", "present": False, "payment": "cash_payment"},
-            follow=True,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["member"], None)
-        self.assertEqual(response.data["name"], "Test 2")
-        reg2.refresh_from_db()
-        self.assertEqual(reg2.payment.type, "cash_payment")
-        self.assertFalse(reg2.present)
 
     def test_mark_present_url_registered(self):
         registration = EventRegistration.objects.create(
@@ -547,3 +227,17 @@ class EventApiV2Test(TestCase):
         response = self.client.get("/api/v2/events/", format="json")
         self.assertEqual(response.data["count"], 1)
         self.assertEqual(response.data["results"][0]["title"], "testevent")
+
+    def test_event_detail(self):
+        response = self.client.get("/api/v2/events/1/", format="json")
+        self.assertEqual(response.data["title"], "testevent")
+
+    def test_event_detail_not_found(self):
+        response = self.client.get("/api/v2/events/2/", format="json")
+        self.assertEqual(response.status_code, 404)
+
+    def test_event_detail_unpublished(self):
+        self.event.published = False
+        self.event.save()
+        response = self.client.get("/api/v2/events/1/", format="json")
+        self.assertEqual(response.status_code, 404)
