24a152471a1c5edf01db71a0dc494b0c56b0f151
Quinten Kock <7569492+ColonelPhantom@users.noreply.github.com>
2024-11-27 19:06:20 +0100

    Add announcements list API and refactor persistent announcements (#3843)
    * Add announcements list api and refactor persistent announcements
    
    * Add pk to serializer
    
    * Add announcement hiding to API

--- website/announcements/api/v2/views.py
+++ website/announcements/api/v2/views.py
@@ -1,10 +1,17 @@
 """API v2 views of the announcements app."""
 
-from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-
-from announcements.api.v2.serializers import FrontpageArticleSerializer, SlideSerializer
+from announcements.api.v2.serializers import (
+    AnnouncementSerializer,
+    FrontpageArticleSerializer,
+    SlideSerializer,
+)
+from announcements.context_processors import announcements
 from announcements.models import FrontpageArticle, Slide
+from announcements.services import close_announcement
+from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 
 class AnnouncementsAPIViewMixin:
@@ -40,3 +47,25 @@ class FrontpageArticleDetailView(AnnouncementsAPIViewMixin, RetrieveAPIView):
 
     serializer_class = FrontpageArticleSerializer
     queryset = FrontpageArticle.visible_objects.all()
+
+
+class AnnouncementListView(AnnouncementsAPIViewMixin, viewsets.ViewSet):
+    """Returns a list of announcements."""
+
+    serializer_class = AnnouncementSerializer
+
+    def list(self, request):
+        announces = announcements(request)
+        serializer = self.serializer_class(
+            announces["announcements"] + announces["persistent_announcements"],
+            many=True,
+        )
+        return Response(serializer.data)
+
+
+class AnnouncementDetailView(AnnouncementsAPIViewMixin, viewsets.ViewSet):
+    serializer_class = AnnouncementSerializer
+
+    def hide(self, request, pk):
+        close_announcement(request, pk)
+        return Response(status=204)
