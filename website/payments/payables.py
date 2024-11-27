93a71f80c27c64f4f2d9497c0577a581c212bf1a
Ties Dirksen <tiesdirksen@gmail.com>
2024-11-27 19:43:08 +0100

    Oops

--- website/payments/payables.py
+++ website/payments/payables.py
@@ -151,21 +151,7 @@ class PaymentRequestPayable(Payable):
         ]
 
     def register(self, model: type[Model], payable_class: type[Payable]):
-        self._registry[self._get_key(model)] = payable_class
-        if payable_class.immutable_after_payment:
-            pre_save.connect(
-                prevent_saving, sender=model, dispatch_uid=f"prevent_saving_{model}"
-            )
-
-            for foreign_model in payable_class.immutable_foreign_key_models:
-                foreign_key_field = payable_class.immutable_foreign_key_models[
-                    foreign_model
-                ]
-                pre_save.connect(
-                    prevent_saving_related(foreign_key_field),
-                    sender=foreign_model,
-                    dispatch_uid=f"prevent_saving_related_{model}_{foreign_model}",
-                )
+        raise NotImplementedError("PaymentRequestPayable cannot be registered.")
 
 
 class Payables:
