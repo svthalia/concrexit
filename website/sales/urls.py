7838176215e43ec59e8a33c53d69c0b6e5a1b5b1
Quinten Kock <quintenkock@gmail.com>
2025-05-11 09:51:17 +0200

    Add basic ordering form

--- website/sales/urls.py
+++ website/sales/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from sales.views import OrderPaymentView
+from sales.views import place_order_view, OrderPaymentView
 
 app_name = "sales"
 
 urlpatterns = [
+    path(
+        "sales/shifts/<int:pk>/order",
+        place_order_view,
+        name="order-place",
+    ),
     path(
         "sales/order/<uuid:pk>/pay/",
         OrderPaymentView.as_view(),
