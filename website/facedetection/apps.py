502ee5815f2f79ce52b0b84ca9f47f1af8d8c486
Ties Dirksen <tiesdirksen@gmail.com>
2025-09-01 12:55:02 +0200

    Move facedetection data minimisation to apps

--- website/facedetection/apps.py
+++ website/facedetection/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.db.models import Q
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -23,3 +26,27 @@ class FaceDetectionConfig(AppConfig):
     def ready(self):
         """Register signals when the app is ready."""
         from . import signals  # noqa: F401
+
+    def execute_data_minimisation(self, dry_run=False):
+        """Delete old reference faces.
+
+        This deletes reference faces that have been marked for deletion by the user for
+        some time, as well as reference faces of users that have not logged in for a year.
+        """
+        from .models import ReferenceFace
+
+        delete_period_inactive_member = timezone.now() - timezone.timedelta(days=365)
+        delete_period_marked_for_deletion = timezone.now() - timezone.timedelta(
+            days=settings.FACEDETECTION_REFERENCE_FACE_STORAGE_PERIOD_AFTER_DELETE_DAYS
+        )
+
+        queryset = ReferenceFace.objects.filter(
+            Q(marked_for_deletion_at__lte=delete_period_marked_for_deletion)
+            | Q(user__last_login__lte=delete_period_inactive_member)
+        )
+
+        if not dry_run:
+            for reference_face in queryset:
+                reference_face.delete()  # Don't run the queryset method, this will also delete the file
+
+        return queryset
