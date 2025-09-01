c3396a2032c15e7370de74b4af8a63cf719510fd
Ties Dirksen <tiesdirksen@gmail.com>
2025-09-01 12:55:59 +0200

    Move pizzas data minimisation to apps

--- website/pizzas/apps.py
+++ website/pizzas/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -12,3 +13,17 @@ class PizzasConfig(AppConfig):
         from .payables import register
 
         register()
+
+    def execute_data_minimisation(self, dry_run=False):
+        """Anonymizes pizzas orders older than 3 years."""
+        # Sometimes years are 366 days of course, but better delete 1 or 2 days early than late
+        from .models import FoodOrder
+
+        deletion_period = timezone.now().date() - timezone.timedelta(days=365 * 3)
+
+        queryset = FoodOrder.objects.filter(
+            food_event__end__lte=deletion_period
+        ).exclude(name="<removed>")
+        if not dry_run:
+            queryset.update(member=None, name="<removed>")
+        return queryset
