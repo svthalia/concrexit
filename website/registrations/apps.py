754f36d760bcff7b8be62ce6162c6fbce001e7fd
Ties Dirksen <tiesdirksen@gmail.com>
2025-09-01 12:56:15 +0200

    Move registration data minimisation to apps

--- website/registrations/apps.py
+++ website/registrations/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.db.models import Q, Value
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -41,3 +43,34 @@ class RegistrationsConfig(AppConfig):
                 },
             ],
         }
+
+    def execute_data_minimisation(self, dry_run=False):
+        """Delete completed or rejected registrations that were modified at least 31 days ago.
+
+        :param dry_run: does not really remove data if True
+        :return: number of removed objects.
+        """
+        from .models import Entry, Registration, Renewal
+
+        deletion_period = timezone.now() - timezone.timedelta(days=31)
+        registrations = Registration.objects.filter(
+            Q(status=Entry.STATUS_COMPLETED) | Q(status=Entry.STATUS_REJECTED),
+            updated_at__lt=deletion_period,
+        )
+        renewals = Renewal.objects.filter(
+            Q(status=Entry.STATUS_COMPLETED) | Q(status=Entry.STATUS_REJECTED),
+            updated_at__lt=deletion_period,
+        )
+
+        if dry_run:
+            return registrations.count() + renewals.count()  # pragma: no cover
+
+        # Mark that this deletion is for data minimisation so that it can be recognized
+        # in any post_delete signal handlers. This is used to prevent the deletion of
+        # Moneybird invoices.
+        registrations = registrations.annotate(
+            __deleting_for_dataminimisation=Value(True)
+        )
+        renewals = renewals.annotate(__deleting_for_dataminimisation=Value(True))
+
+        return registrations.delete()[0] + renewals.delete()[0]
