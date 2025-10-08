e3f66e9f2071bcd9c1795f872ba51a47a455c26e
annalorenz123 <annalorenz@gmail.com>
2025-10-08 19:47:43 +0200

    Added event cards on frontpage of events with open registration

--- website/events/templatetags/event_cards.py
+++ website/events/templatetags/event_cards.py
@@ -1,6 +1,7 @@
 from django import template
 
 from events.templatetags.frontpage_events import render_frontpage_events
+from events.templatetags.frontpage_events import render_frontpage_registration_events
 
 register = template.Library()
 
@@ -8,3 +9,8 @@ register = template.Library()
 @register.inclusion_tag("events/event_cards.html", takes_context=True)
 def render_event_cards(context, events=None):
     return render_frontpage_events(context, events)
+
+
+@register.inclusion_tag("events/registration_events.html", takes_context=True)
+def render_registration_event_cards(context, events=None):
+    return render_frontpage_registration_events(context, events)
